abeee997d41e3071520cab985028317992179e90
Huse Kivrak <huse7@pm.me>
2024-01-12 14:03:21 -0500

    feature - create voice clone from formData through server action

--- app/create-clone/page.tsx
+++ app/create-clone/page.tsx
@@ -1,14 +1,26 @@
+'use client';
+import { SubmitButton } from '@/components/SubmitButton';
+import { useFormState } from 'react-dom';
 import { CreateVoiceClone } from '@/utils/elevenlabs/actions';
 
+const initialState = {
+message: '',
+};
+
 export default function CreateClone() {
+  const [state, formAction] = useFormState(CreateVoiceClone, initialState);
+
   return (
     <div>
       <h1>Create A Clone</h1>
-      <form action={CreateVoiceClone}>
+      <form action={formAction}>
         <input type='text' name='name' placeholder='Name' required />
         <input type='text' name='description' placeholder='Description' />
         <input type='file' name='file' accept='audio/*' required />
-        <button type='submit'>clone me</button>
+        <SubmitButton />
+        <p aria-live='polite' className='sr-only' role='status'>
+          {state?.message}
+        </p>
       </form>
     </div>
   );
--- utils/elevenlabs/actions.ts
+++ utils/elevenlabs/actions.ts
@@ -1,21 +1,33 @@
 'use server';
-export async function CreateVoiceClone(formData: FormData) {
-  try {
-    const BASE_URL = 'https://api.elevenlabs.io/v1';
-    const ELEVENLABS_API_KEY = process.env.ELEVENLABS_API_KEY ? process.env.ELEVENLABS_API_KEY : '';
+import { createClient } from '../supabase/server';
+import { cookies } from 'next/headers';
+export async function CreateVoiceClone(prevState: any, formData: FormData) {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
 
+  //todo: add audio file to db
+
+  console.log('formData: ', formData);
+  const BASE_URL = 'https://api.elevenlabs.io/v1';
+  const ELEVENLABS_API_KEY = process.env.ELEVENLABS_API_KEY ? process.env.ELEVENLABS_API_KEY : '';
+
+  try {
     const response = await fetch(`${BASE_URL}/voices/add`, {
       method: 'POST',
       headers: {
+        //? fetch API handles Content-Type (boundary error otherwise)
         'Xi-Api-Key': ELEVENLABS_API_KEY,
-        'Content-Type': 'multipart/form-data',
       },
       body: formData,
     });
 
-    return response.json();
+    const data = await response.json();
+    console.log('voice:', data.voice_id);
+
+    //todo: add voiceId to db
+
+    return { message: 'Voice clone created successfully' };
   } catch (error) {
-    console.error(error);
-    return null;
+    return { message: `Error: ${error}` };
   }
 }
