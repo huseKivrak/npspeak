923705dde091799a1682058c285681104cb9179c
Huse Kivrak <huse7@pm.me>
2024-01-13 16:30:48 -0500

    refactor - moved api constants

--- utils/elevenlabs/actions.ts
+++ utils/elevenlabs/actions.ts
@@ -1,23 +1,19 @@
 'use server';
 import { createClient } from '../supabase/server';
 import { cookies } from 'next/headers';
+
+import { BASE_URL, ELEVENLABS_API_HEADERS } from './api';
 export async function CreateVoiceClone(prevState: any, formData: FormData) {
+  //todo: add audio file to db
   const cookieStore = cookies();
   const supabase = createClient(cookieStore);
 
-  //todo: add audio file to db
-
   console.log('formData: ', formData);
-  const BASE_URL = 'https://api.elevenlabs.io/v1';
-  const ELEVENLABS_API_KEY = process.env.ELEVENLABS_API_KEY ? process.env.ELEVENLABS_API_KEY : '';
 
   try {
     const response = await fetch(`${BASE_URL}/voices/add`, {
       method: 'POST',
-      headers: {
-        //? fetch API handles Content-Type (boundary error otherwise)
-        'Xi-Api-Key': ELEVENLABS_API_KEY,
-      },
+      headers: ELEVENLABS_API_HEADERS,
       body: formData,
     });
 
@@ -27,6 +23,27 @@ export async function CreateVoiceClone(prevState: any, formData: FormData) {
     //todo: add voiceId to db
 
     return { message: 'Voice clone created successfully' };
+  } catch (error) {
+    return { message: `Error: ${error}` };
+  }
+}
+
+export async function CreateAudioClip(prevState: any, formData: FormData) {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+
+  const voice_id = formData.get('voice_id');
+  formData.set('model_id', 'eleven_multilingual_v2');
+
+  try {
+    const response = await fetch(`${BASE_URL}/text-to-speech/${voice_id}`, {
+      method: 'POST',
+      headers: ELEVENLABS_API_HEADERS,
+      body: formData,
+    });
+
+    const data = await response.json();
+    console.log('clip:', data.clip_id);
   } catch (error) {
     return { message: `Error: ${error}` };
   }
--- utils/elevenlabs/api.ts
+++ utils/elevenlabs/api.ts
@@ -1,3 +1,23 @@
 //docs - https://elevenlabs.io/docs/api-reference/
 
-// const BASE_URL = 'https://api.elevenlabs.io/v1/';
+const ELEVENLABS_API_KEY = process.env.ELEVENLABS_API_KEY ? process.env.ELEVENLABS_API_KEY : '';
+
+export const BASE_URL = 'https://api.elevenlabs.io/v1';
+
+export const ELEVENLABS_API_HEADERS = {
+  'Xi-Api-Key': ELEVENLABS_API_KEY,
+};
+
+export const audioFormats = {
+  lowQuality22kHz: 'mp3_22050_32',
+  standardQuality44kHzLowBitrate: 'mp3_44100_32',
+  mediumQuality44kHz: 'mp3_44100_64',
+  highQuality44kHzMediumBitrate: 'mp3_44100_96',
+  veryHighQuality44kHz: 'mp3_44100_128', // Default
+  professionalQuality44kHz: 'mp3_44100_192',
+  rawAudio16kHz: 'pcm_16000',
+  rawAudio22kHz: 'pcm_22050',
+  rawAudio24kHz: 'pcm_24000',
+  cdQualityRawAudio: 'pcm_44100',
+  telephonyQuality: 'ulaw_8000',
+};
