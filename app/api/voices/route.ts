499292f77f9212096c096b9a8f8b50bed6e842b8
Huse Kivrak <huse7@pm.me>
2024-01-13 16:19:25 -0500

    refactor - cleaned up fetch

--- app/api/voices/route.ts
+++ app/api/voices/route.ts
@@ -1,18 +1,15 @@
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 const BASE_URL = 'https://api.elevenlabs.io/v1';
 const ELEVENLABS_API_KEY = process.env.ELEVENLABS_API_KEY ? process.env.ELEVENLABS_API_KEY : '';
 
-const options: RequestInit = {
-  headers: {
-    'Content-Type': 'application/json',
-    'Xi-Api-Key': ELEVENLABS_API_KEY,
-  },
-};
-
 export const GET = async (request: NextRequest) => {
-  const res = await fetch(`${BASE_URL}/voices`, options);
-  return res.json();
+  const res = await fetch(`${BASE_URL}/voices`, {
+    headers: {
+      'Xi-Api-Key': ELEVENLABS_API_KEY,
+    },
+  });
+  const voices = await res.json();
+  console.log('voices from API route:', voices);
+  return NextResponse.json(voices);
 };
-
-
