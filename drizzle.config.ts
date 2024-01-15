f9346593716e109c96a9e96bce5c5d7dfea54dbd
Huse Kivrak <huse7@pm.me>
2024-01-15 15:26:19 -0500

    fix - manually loaded .env for drizzle variables. still introspecting auth table for profiles.user link..

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,11 @@
 import type { Config } from 'drizzle-kit';
+import { cwd } from 'process';
+import { loadEnvConfig } from '@next/env';
+
+loadEnvConfig(cwd());
 
 const pw = process.env.DATABASE_PASSWORD || '';
-
+const dbHost = process.env.DATABASE_HOST || '';
 
 export default {
   schema: './app/schema/*',
@@ -11,7 +15,7 @@ export default {
   dbCredentials: {
     user: 'postgres',
     password: pw,
-    host: 'db.vjyydfwpiotwefkntrtk.supabase.co',
+    host: dbHost,
     port: 5432,
     database: 'postgres',
   },
