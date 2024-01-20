00ef8eefc5aec0167a7f3f1bc2333ef72f2c4279
Huse Kivrak <huse7@pm.me>
2024-01-20 18:13:15 -0500

    file - moved navbar to components folder; daisyui boilerplate

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import { GeistSans } from 'geist/font/sans';
-import AuthButton from '@/components/buttons/AuthButton';
 import './globals.css';
-import Link from 'next/link';
+import NavBar from '@/components/layout/NavBar';
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -17,12 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang='en' className={GeistSans.className}>
       <body className='bg-background text-foreground'>
-        <nav className='w-full flex justify-center border-b border-b-secondary h-16'>
-          <div className='w-full max-w-4xl flex justify-between items-center p-3 text-sm'>
-            <Link href='/'>npSpeaks</Link>
-            <AuthButton />
-          </div>
-        </nav>
+        <NavBar />
         <main className='min-h-screen flex flex-col items-center'>{children}</main>
       </body>
     </html>
