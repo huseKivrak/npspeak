b71fed3cf133cedc9c48a09fe6113affb54a045b
Huse Kivrak <huse7@pm.me>
2024-01-21 17:16:49 -0500

    feature - boilerplate daisyui footer, hero

---
+++ components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className='footer footer-center p-4 bg-base-300 text-base-content'>
+      <aside>
+        <p>Copyright © 2024 - All rights reserved by npSpeak</p>
+      </aside>
+    </footer>
+  );
+}
