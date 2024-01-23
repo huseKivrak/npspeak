6fd7a7e26f413de3ae9a114c71acbb053c7b2c4f
Huse Kivrak <huse7@pm.me>
2024-01-22 21:57:38 -0500

    feature - utility function for getting user from session

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -30,3 +30,17 @@ export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
     }
   );
 };
+
+export const getUserFromSession = async () => {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+  try {
+    const {
+      data: { session },
+    } = await supabase.auth.getSession();
+    return session?.user;
+  } catch (error) {
+    console.error('Error:', error);
+    return null;
+  }
+};
