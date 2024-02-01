a670f856cf6bf1a3c592458f5a3afec92b58c7e2
Huse Kivrak <huse7@pm.me>
2024-01-31 21:40:23 -0500

    file - moved boilerplate supabase functions to their own folder, with separate helpers file for utils

--- utils/supabase/client.ts
+++
@@ -1,8 +0,0 @@
-import { createBrowserClient } from '@supabase/ssr';
-
-//essentially createClient + Singleton pattern; avoids creating multiple clients
-export const createClient = () =>
-  createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
