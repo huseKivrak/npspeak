af734fc47ec851c3e1d1e89d96c56fa6c3c6b036
Huse Kivrak <huse7@pm.me>
2024-02-03 04:50:07 -0500

    minor - replaced ` ' ` with html entity, autoformat changes

--- app/[username]/campaigns/page.tsx
+++ app/[username]/campaigns/page.tsx
@@ -1,36 +1,38 @@
 'use client';
-import { useState, useEffect } from 'react';
-import { getCampaignsAndNPCs, GroupedResults } from '@/utils/drizzle/helpers';
-import { usePathname } from 'next/navigation';
+import {useState, useEffect} from 'react';
+import {getCampaignsAndNPCs, GroupedResults} from '@/utils/drizzle/helpers';
+import {usePathname} from 'next/navigation';
 import CampaignCard from '@/components/cards/CampaignCard';
 import NPCCarousel from '@/components/NPCCarousel';
 
 export default function UserCampaignsPage() {
-  const [userData, setUserData] = useState<GroupedResults>([]);
-  const path = usePathname();
-  const username = path.split('/').filter(Boolean)[0];
+	const [userData, setUserData] = useState<GroupedResults>([]);
+	const path = usePathname();
+	const username = path.split('/').filter(Boolean)[0];
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await getCampaignsAndNPCs();
-      if (data) {
-        setUserData(data);
-      }
-    };
+	useEffect(() => {
+		const fetchData = async () => {
+			const data = await getCampaignsAndNPCs();
+			if (data) {
+				setUserData(data);
+			}
+		};
 
-    fetchData();
-  }, []);
+		fetchData();
+	}, []);
 
-  return (
-    <div className='flex flex-col items-center'>
-      <h1 className='text-4xl tracking-widest font-extralight my-4'>{username}'s campaigns</h1>
-      <ul className='flex flex-col w-full lg:flex-row gap-4'>
-        {userData.map((item) => (
-          <li key={item.campaign.id}>
-            <CampaignCard campaign={item.campaign} NPCs={item.npcs} />
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+	return (
+		<div className='flex flex-col items-center'>
+			<h1 className='text-4xl tracking-widest font-extralight my-4'>
+				{username}&apos;s campaigns
+			</h1>
+			<ul className='flex flex-col w-full lg:flex-row gap-4'>
+				{userData.map((item) => (
+					<li key={item.campaign.id}>
+						<CampaignCard campaign={item.campaign} NPCs={item.npcs} />
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 }
