18ddba579aebcae83ecd9b670efbecc239d41019
Huse Kivrak <huse7@pm.me>
2024-02-03 04:39:37 -0500

    feature - refactored createCampaignAction for cleaner syntax, error handling. added a few comments

--- database/drizzle/queries.ts
+++ database/drizzle/queries.ts
@@ -1,46 +1,81 @@
 'use server';
-import { redirect } from 'next/navigation';
-import { db } from '.';
-import { getUserFromSession } from '@/utils/supabase/helpers';
-import { campaigns } from './schema';
-import { insertCampaignSchema } from './schema';
-import { ZodError } from 'zod';
-import { revalidatePath } from 'next/cache';
+import {redirect} from 'next/navigation';
+import {db} from '.';
+import {getUserFromSession, getUsername} from '@/utils/supabase/helpers';
+import {campaigns, npcs} from './schema';
+import {insertCampaignSchema, insertNPCSchema} from './schema';
+import {ZodError} from 'zod';
+import {revalidatePath} from 'next/cache';
 
-export const createCampaignAction = async (prevState: any, formData: FormData) => {
-  const user = await getUserFromSession();
-  if (!user) {
-    return redirect('/');
-  }
+//Inserts campaigns into db and redirects to its detail page
+export const createCampaignAction = async (
+	prevState: any,
+	formData: FormData
+) => {
+	const user = await getUserFromSession();
+	if (!user) redirect('/');
 
-  let newCampaignId;
+	const username = getUsername(user);
 
-  try {
-    const campaign_name = formData.get('campaign_name') as string;
-    const description = formData.get('description') as string;
-    const start_date = formData.get('start_date');
-    const end_date = formData.get('end_date');
+	let newCampaignId: number;
+	const campaign_name = formData.get('campaign_name');
+	const description = formData.get('description');
+	const start_date = formData.get('start_date');
+	const end_date = formData.get('end_date');
 
-    const validatedData = insertCampaignSchema.parse({
-      user_id: user.id,
-      is_default: false,
-      campaign_name: campaign_name,
-      description: description,
-      start_date: start_date === '' ? null : start_date, //*
-      end_date: end_date === '' ? null : end_date, //*
-    });
+	try {
+		const validatedData = insertCampaignSchema.parse({
+			user_id: user.id,
+			// is_default: false,
+			campaign_name,
+			description,
+			start_date: start_date === '' ? null : start_date, //for weird date type errors
+			end_date: end_date === '' ? null : end_date,
+		});
 
-    const data = await db.insert(campaigns).values(validatedData).returning();
-    newCampaignId = data[0].id;
+		const insertedCampaignId = await db
+			.insert(campaigns)
+			.values(validatedData)
+			.returning({id: campaigns.id});
+		newCampaignId = insertedCampaignId[0].id;
+	} catch (error: any) {
+		return error instanceof ZodError
+			? {zodError: error.errors}
+			: {message: error.message};
+	}
 
-  } catch (error: any) {
-    if (error instanceof ZodError) {
-      return { zodError: error.errors };
-    } else {
-      return { message: [error.message] };
-    }
-  }
+	//* must be outside try/catch:
+	revalidatePath(`/${username}/campaigns`);
+	redirect(`/${username}/campaigns/${newCampaignId}`);
+};
 
-  revalidatePath(`/${user.user_metadata.username}/campaigns`);
-  redirect(`/${user.user_metadata.username}/campaigns/${newCampaignId}`);
+//Inserts NPCs into db then redirects to its detail page
+export const createNPCAction = async (prevState: any, formData: FormData) => {
+	const user = await getUserFromSession();
+	if (!user) redirect('/');
+
+	const username = getUsername(user);
+	let newNPCId;
+
+	try {
+		const npc_name = formData.get('npc_name');
+		const description = formData.get('description');
+
+		const validatedData = insertNPCSchema.parse({
+			npc_name,
+			description,
+			user_id: user.id,
+		});
+
+		const insertedNPCId = await db
+			.insert(npcs)
+			.values(validatedData)
+			.returning({id: npcs.id});
+		newNPCId = insertedNPCId[0].id;
+	} catch (error: any) {
+		if (error instanceof ZodError) return {zodError: error.errors};
+		return {message: error.message};
+	}
+	revalidatePath(`/${username}/npcs/`);
+	redirect(`/${username}/npcs/${newNPCId}`);
 };
