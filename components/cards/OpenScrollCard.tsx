c4d3a5c34331fc9144a73429683488ca65b46f62
Huse Kivrak <huse7@pm.me>
2024-02-07 19:49:29 -0500

    refactor - simplified image source ternary, other minor edits

--- components/cards/OpenScrollCard.tsx
+++ components/cards/OpenScrollCard.tsx
@@ -14,32 +14,29 @@ export default function OpenScrollCard({
 
 	return (
 		<div className='relative w-96 bg-transparent'>
-			{showNPCs ? (
-				<img
-					src='/images/scrolls/open_scroll_no_bg.png'
-					alt='scroll'
-					className='absolute inset-0 w-full h-full object-cover'
-				/>
-			) : (
-				<img
-					src='/images/scrolls/closed_scroll_no_bg.png'
-					alt='scroll'
-					className='absolute inset-0 w-full h-full object-cover'
-				/>
-			)}
-			<div className='relative z-10 p-4 left-0.5 translate-x-48'>
-				<h2 className='text-2xl font-bold text-gray-800 mt-8 px-8'>
+			<img
+				src={`/images/scrolls${
+					showNPCs ? '/open_scroll_no_bg.png' : '/closed_scroll_no_bg.png'
+				}`}
+				alt='scroll'
+				className='absolute inset-0 w-full h-full object-cover'
+			/>
+
+			<div className='relative z-10 p-4'>
+				<h2 className='text-2xl font-bold text-gray-800 mt-8 px-16'>
 					{campaign.campaign_name}
 				</h2>
 				{campaign.description && (
-					<p className='text-stone-600 p-8'>{campaign.description}</p>
+					<p className='text-stone-100 p-10'>{campaign.description}</p>
 				)}
-				<button
-					className='mt-4 btn text-white btn-ghost'
-					onClick={() => setShowNPCs(!showNPCs)}
-				>
-					See NPCs
-				</button>
+				<div className='flex justify-center'>
+					<button
+						className='btn btn-sm text-white btn-ghost mb-8'
+						onClick={() => setShowNPCs(!showNPCs)}
+					>
+						NPCs
+					</button>
+				</div>
 				{showNPCs && NPCs && <NPCCarousel NPCs={NPCs} />}
 			</div>
 		</div>
