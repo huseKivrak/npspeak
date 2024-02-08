52467b1a65c982625c493d35feb7b43a1d4d1320
Huse Kivrak <huse7@pm.me>
2024-02-07 19:48:51 -0500

    feature - added filler favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,34 @@
-import { GeistSans } from 'geist/font/sans';
+import {GeistSans} from 'geist/font/sans';
 import './globals.css';
 import NavBar from '@/components/layout/NavBar';
 import Footer from '@/components/layout/Footer';
-import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import {Analytics} from '@vercel/analytics/react';
+import {SpeedInsights} from '@vercel/speed-insights/next';
 
 const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : 'http://localhost:3000';
+	? `https://${process.env.VERCEL_URL}`
+	: 'http://localhost:3000';
 
 export const metadata = {
-  metadataBase: new URL(defaultUrl),
-  title: 'npSpeak',
+	metadataBase: new URL(defaultUrl),
+	title: 'npSpeak',
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang='en' className={GeistSans.className}>
-      <body className=''>
-        <NavBar />
-        <main className='min-h-screen flex flex-col items-center'>
-          {children}
-          <Analytics />
-          <SpeedInsights/>
-        </main>
-        <Footer />
-      </body>
-    </html>
-  );
+export default function RootLayout({children}: {children: React.ReactNode}) {
+	return (
+		<html lang='en' className={GeistSans.className}>
+			<head>
+				<link rel='icon' href='/favicon.ico' sizes='any' />
+			</head>
+			<body className=''>
+				<NavBar />
+				<main className='min-h-screen flex flex-col items-center'>
+					{children}
+					<Analytics />
+					<SpeedInsights />
+				</main>
+				<Footer />
+			</body>
+		</html>
+	);
 }
