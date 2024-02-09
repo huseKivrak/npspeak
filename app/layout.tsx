cb6e0281ae65af1d1f8cb17976f0d4d6f857fe03
Huse Kivrak <huse7@pm.me>
2024-02-08 22:01:59 -0500

    minor - background experimenting

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import {GeistSans} from 'geist/font/sans';
 import './globals.css';
-import NavBar from '@/components/layout/NavBar';
+import NavBar from '@/components/layout/NavBar.1';
 import Footer from '@/components/layout/Footer';
 import {Analytics} from '@vercel/analytics/react';
 import {SpeedInsights} from '@vercel/speed-insights/next';
@@ -20,7 +20,7 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
 			<head>
 				<link rel='icon' href='/favicon.ico' sizes='any' />
 			</head>
-			<body className=''>
+			<body className='bg-gradient-to-t from-green-950 via-green-700 to-green-800'>
 				<NavBar />
 				<main className='min-h-screen flex flex-col items-center'>
 					{children}
