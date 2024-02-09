228c5814a4ba36df6318530b5b78259dfad0300b
Huse Kivrak <huse7@pm.me>
2024-02-08 22:04:20 -0500

    minor - fixed import typo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import {GeistSans} from 'geist/font/sans';
 import './globals.css';
-import NavBar from '@/components/layout/NavBar.1';
+import NavBar from '@/components/layout/NavBar';
 import Footer from '@/components/layout/Footer';
 import {Analytics} from '@vercel/analytics/react';
 import {SpeedInsights} from '@vercel/speed-insights/next';
