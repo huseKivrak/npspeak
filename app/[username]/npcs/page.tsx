5d6c89b822f0b959036897fe9fd79af9d9e66c9a
Huse Kivrak <huse7@pm.me>
2024-02-13 17:57:33 -0500

    feature - boilerplate NPC list page

--- app/[username]/npcs/page.tsx
+++ app/[username]/npcs/page.tsx
@@ -1,10 +1,20 @@
-import NPCForm from '@/components/forms/NPCForm';
+import {getNPCsAction} from '@/server-actions/drizzle';
+import NPCCard from '@/components/cards/NPCCard';
 
-export default function NPCsPage() {
+export default async function NPCsPage({params}: {params: {username: string}}) {
+	const npcs = await getNPCsAction();
+	const username = params.username;
 	return (
 		<div>
-			<h1>npcs</h1>
-			<NPCForm />
+			<h1>{username}&apos;s npcs</h1>
+			<ul>
+				{npcs &&
+					npcs.map((n) => (
+						<li key={n.npc.id}>
+							<NPCCard npcData={n} />
+						</li>
+					))}
+			</ul>
 		</div>
 	);
 }
