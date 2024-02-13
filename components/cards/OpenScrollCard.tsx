352729e1c69592a3b7005f0eb3fdaa674e3167d3
Huse Kivrak <huse7@pm.me>
2024-02-13 17:56:27 -0500

    refactor - fixed prop for card and used next's Image instead of img element

--- components/cards/OpenScrollCard.tsx
+++ components/cards/OpenScrollCard.tsx
@@ -1,46 +1,51 @@
 'use client';
 import {Tables} from '@/types/supabase';
 import {useState} from 'react';
-import NPCCarousel from '../NPCCarousel';
+import NPCCarousel from './NPCCarousel';
+import {CampaignsWithNPCs} from '@/server-actions/drizzle';
+import Image from 'next/image';
 
 //todo
 import Link from 'next/link';
 
 export default function OpenScrollCard({
-	campaign,
-	NPCs,
+	campaignData,
 }: {
-	campaign: Tables<'campaigns'>;
-	NPCs?: Tables<'npcs'>[] | null;
+	campaignData: CampaignsWithNPCs;
 }) {
 	const [showNPCs, setShowNPCs] = useState(false);
+	const {campaign, npcs} = campaignData;
 
 	return (
-		<div className='relative w-80 bg-transparent'>
-			<img
+		<div className='relative w-full bg-transparent m-12'>
+			<Image
 				src={`/images/scrolls${
 					showNPCs ? '/open_scroll_no_bg.png' : '/closed_scroll_no_bg.png'
 				}`}
 				alt='Scroll'
 				className='absolute inset-0 w-full h-full object-cover'
 				style={{filter: 'brightness(0.6)'}}
+				layout='fill'
 			/>
 
 			<div className='relative z-10 text-center'>
-				<h2 className='mt-2 text-xl text-transparent bg-clip-text bg-gradient-to-br from-yellow-50 via-yellow-500 to-yellow-100 font-extralight hover:underline'>
+				<h2 className=' text-xl text-transparent bg-clip-text bg-gradient-to-br from-yellow-50 via-yellow-500 to-yellow-100 font-extralight hover:underline'>
 					{campaign.campaign_name}
 				</h2>
 				{showNPCs ? (
-					<p className='text-stone-100 p-10'>{campaign.description}</p>
+					<p className='text-balance text-stone-100 text-sm p-10 text-center'>
+						{campaign.description}
+					</p>
 				) : (
 					<button
-						className='btn btn-xs text-white btn-ghost'
+						className='btn btn-xs px-0 text-white btn-ghost'
 						onClick={() => setShowNPCs(!showNPCs)}
 					>
 						NPCs
 					</button>
 				)}
-				{showNPCs && NPCs && <NPCCarousel NPCs={NPCs} />}
+				{showNPCs && npcs && <NPCCarousel NPCs={npcs} />}
+				{/* {showNPCs && NPCs && <Responsive NPCs={NPCs} />} */}
 			</div>
 		</div>
 	);
