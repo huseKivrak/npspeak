dbe1d38d15bf8dd27b1a4dc50aea6ca63db5e474
Huse Kivrak <huse7@pm.me>
2024-02-14 12:02:21 -0500

    refactor - user landing page is now a server component that fetches data for dumb client component

--- app/[username]/page.tsx
+++ app/[username]/page.tsx
@@ -1,55 +1,24 @@
-'use client';
-
-import {createClient} from '@/utils/supabase/defaults/client';
-import {getUsername} from '@/server-actions/auth';
-import {useRouter} from 'next/navigation';
+import UserDashboard from '@/components/UserDashboard';
+import {getUserFromSession, getUsername} from '@/server-actions/auth';
+import {redirect} from 'next/navigation';
+import {getCampaignsAndNPCs, getNPCsAction} from '@/server-actions/drizzle';
 import CampaignForm from '@/components/forms/CampaignForm';
-import {useState, useEffect} from 'react';
 import Link from 'next/link';
 
-export default function UserPage({params}: {params: {username: string}}) {
-	const [showCampaignForm, setShowCampaignForm] = useState(false);
-	const [authUsername, setAuthUsername] = useState<string | null>(null);
-	const router = useRouter();
+export default async function UserPage({params}: {params: {username: string}}) {
+	const user = await getUserFromSession();
+	if (!user) redirect('/login');
 
-	useEffect(() => {
-		const fetchUserData = async () => {
-			const supabase = createClient();
+	const sessionUsername = user?.user_metadata.username;
+	if (sessionUsername.toLowerCase() !== params.username.toLowerCase())
+		redirect('/login');
 
-			try {
-				const {
-					data: {session},
-				} = await supabase.auth.getSession();
-				if (session) {
-					const user = session.user;
-					const username = await getUsername(user);
+	const campaigns = await getCampaignsAndNPCs();
+	const npcs = await getNPCsAction();
 
-					//redirect if route username doesn't match authenticated user (reserves '/your-username' for that user)
-					if (username.toLowerCase() !== params.username.toLowerCase()) {
-						router.push('/');
-					}
-					setAuthUsername(username);
-				} else {
-					router.push('/');
-				}
-			} catch (error) {
-				console.error('Error: ', error);
-				router.push('/');
-			}
-		};
-		fetchUserData();
-	}, [params.username, router]);
 	return (
-		<div className='flex flex-col gap-4'>
-			<h1 className=''>hi, {authUsername}</h1>
-			<Link href={`/${authUsername}/campaigns`}>my campaigns</Link>
-			<button
-				className='btn btn-ghost'
-				onClick={() => setShowCampaignForm((prevState) => !prevState)}
-			>
-				create a new campaign
-			</button>
-			{showCampaignForm && <CampaignForm />}
+		<div>
+			<UserDashboard user={user} campaigns={campaigns} npcs={npcs} />
 		</div>
 	);
 }
---
+++ components/UserDashboard.tsx
@@ -0,0 +1,57 @@
+'use client';
+import {CampaignsWithNPCs, NPCsWithCampaigns} from '@/server-actions/drizzle';
+import {User} from '@supabase/supabase-js';
+import {useState} from 'react';
+import CampaignForm from './forms/CampaignForm';
+import NPCForm from './forms/NPCForm';
+
+export default function UserDashboard({
+	user,
+	campaigns,
+	npcs,
+}: {
+	user: User;
+	campaigns: CampaignsWithNPCs[] | null;
+	npcs: NPCsWithCampaigns[] | null;
+}) {
+	const [showCampaignForm, setShowCampaignForm] = useState(false);
+	const [showNPCForm, setShowNPCForm] = useState(false);
+
+	return (
+		<div className='flex flex-col gap-2'>
+			<h1 className='text-3xl'>
+				{user.user_metadata.username}&apos;s dashboard
+			</h1>
+			<ul>
+				<h2 className='text-2xl underline tracking-widest'>campaigns</h2>
+				{campaigns?.map((c) => (
+					<li key={c.campaign.id}>
+						<p>{c.campaign.campaign_name}</p>
+					</li>
+				))}
+			</ul>
+			<button
+				className='btn btn-primary btn-sm'
+				onClick={() => setShowCampaignForm(!showCampaignForm)}
+			>
+				add a new campaign
+			</button>
+			{showCampaignForm && <CampaignForm />}
+			<ul>
+				<h2 className='text-2xl underline tracking-widest'>NPCs</h2>
+				{npcs?.map((n) => (
+					<li key={n.npc.id}>
+						<p>{n.npc.npc_name}</p>
+					</li>
+				))}
+			</ul>
+			<button
+				className='btn btn-primary btn-sm'
+				onClick={() => setShowNPCForm(!showNPCForm)}
+			>
+				add a new NPC
+			</button>
+			{showNPCForm && <NPCForm />}
+		</div>
+	);
+}
