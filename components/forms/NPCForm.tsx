a1a6ada71310b59b93fb6fa4134190d2e4014d08
Huse Kivrak <huse7@pm.me>
2024-02-14 12:04:38 -0500

    refactor / style - form now mimics campaign form in style and function

--- components/forms/NPCForm.tsx
+++ components/forms/NPCForm.tsx
@@ -8,20 +8,39 @@ import ErrorToast from '../ErrorToast';
 export default function NPCForm() {
 	const [state, formAction] = useFormState(createNPCAction, null);
 
+	const getFieldError = (fieldName: string) => {
+		return state?.zodError?.find((err) => err.path[0] === fieldName)?.message;
+	};
+
 	return (
-		<div>
-			<h1>create an NPC</h1>
-			<form action={formAction}>
+		<div className='flex flex-col items-center'>
+			<h1 className='my-8'>create an NPC</h1>
+			<form action={formAction} className='flex flex-col gap-2 w-full max-w-xs'>
+				<label htmlFor='npc_name' className='form-control'>
+					name (required)
+				</label>
 				<input
 					type='text'
 					name='npc_name'
 					placeholder='what are they called?'
+					className='input input-bordered input-primary mb-4'
+					required
 				/>
+				{getFieldError('npc_name') && (
+					<ErrorToast message={getFieldError('npc_name')} />
+				)}
+				<label htmlFor='description' className='form-control'>
+					description
+				</label>
 				<input
 					type='textarea'
 					name='description'
-					placeholder='"brave, yet soggy", "think interior of an orc, exterior of a hobbit"'
+					placeholder='describe your NPC'
+					className='textarea textarea-primary w-full h-24'
 				/>
+				{getFieldError('description') && (
+					<ErrorToast message={getFieldError('description')} />
+				)}
 				<SubmitButton text='create' />
 				{state?.zodError && <ErrorToast message={state.message} />}
 			</form>
