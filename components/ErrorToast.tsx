11116df10e757e9e9552001958aec50a2804fb32
Huse Kivrak <huse7@pm.me>
2024-02-18 22:56:37 -0500

    refactor - simplified ErrorToast to work with rhf's ErrorMessage

--- components/ErrorToast.tsx
+++ components/ErrorToast.tsx
@@ -1,8 +1,3 @@
-type ErrorToastProps = {
-  message?: string;
-};
-
-export default function ErrorToast({ message }: ErrorToastProps) {
-  if (!message) return null;
-  return <div className='text-xs text-red-600 ml-2'>{message}</div>;
+export default function ErrorToast({message}: {message: string}) {
+	return <div className='text-xs text-red-600 ml-2'>{message}</div>;
 }
