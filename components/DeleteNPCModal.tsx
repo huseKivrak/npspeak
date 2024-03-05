8455e639efb8fe61956e3c537d5b4b40179fb02c
Huse Kivrak <huse7@pm.me>
2024-03-05 18:52:03 -0500

    refactor / style - more reusable, fixed typo, etc.

--- components/DeleteNPCModal.tsx
+++ components/DeleteNPCModal.tsx
@@ -4,8 +4,19 @@ import {useEffect, useRef} from 'react';
 import {useFormState} from 'react-dom';
 import {deleteNPCAction} from '@/actions/drizzle/NPCs';
 import {SubmitButton} from './buttons/SubmitButton';
+import {PiSkullBold} from 'react-icons/pi';
+import {cn} from '@/utils/helpers/clsxMerge';
 
-export default function DeleteNPCModal({id}: {id: number}) {
+export default function DeleteNPCModal({
+	id,
+	className,
+	children,
+	...props
+}: {
+	id: number;
+	className?: string;
+	children?: React.ReactNode;
+}) {
 	const [state, formAction] = useFormState(deleteNPCAction, null);
 	const dialogRef = useRef<HTMLDialogElement>(null);
 
@@ -27,14 +38,18 @@ export default function DeleteNPCModal({id}: {id: number}) {
 
 	return (
 		<>
-			<button className='btn btn-xs btn-error' onClick={openModal}>
-				Delete
+			<button
+				onClick={openModal}
+				className={cn('btn btn-sm btn-danger', className)}
+				{...props}
+			>
+				{children}
 			</button>
 			<dialog ref={dialogRef} className='modal modal-bottom sm:modal-middle'>
 				<div className='modal-box'>
 					<h3 className='font-bold text-lg'>Delete NPC</h3>
 					<p className='py-4'>
-						Are you sure you want to delete this NPC and all of it's dialogue?
+						Are you sure you want to delete this NPC and all of its dialogue?
 						This action cannot be undone.
 					</p>
 					<div className='modal-action'>
@@ -43,7 +58,7 @@ export default function DeleteNPCModal({id}: {id: number}) {
 						</form>
 						<form action={formAction}>
 							<input type='hidden' name='npc_id' value={id} />
-							<SubmitButton className='btn btn-sm btn-warning' text='Delete' />
+							<SubmitButton className='btn btn-sm btn-error' text='Delete' />
 						</form>
 					</div>
 				</div>
