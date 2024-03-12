ecac1b9b0e2ab02ad5dca504d7375c97af8c43f7
Huse Kivrak <huse7@pm.me>
2024-03-11 20:23:21 -0400

    refactor - replaced by route handler but updated naming, structure

--- actions/elevenLabs.ts
+++ actions/elevenLabs.ts
@@ -1,17 +1,20 @@
 'use server';
-import fs from 'fs';
+
 import {v4 as uuidv4} from 'uuid';
-import {BASE_URL, ELEVENLABS_API_HEADERS} from '../utils/elevenlabs/api';
+import {
+	ELEVENLABS_BASE_URL,
+	ELEVENLABS_API_HEADERS,
+} from '../utils/elevenlabs/api';
+import {ActionStatus} from '@/types/drizzle';
 
-export async function CreateVoiceClone(prevState: any, formData: FormData) {
-	//todo: add audio file to db
-	// const cookieStore = cookies();
-	// const supabase = createClient(cookieStore);
-
+export async function createElevenLabsVoiceAction(
+	prevState: any,
+	formData: FormData
+): Promise<ActionStatus> {
 	console.log('formData: ', formData);
 
 	try {
-		const response = await fetch(`${BASE_URL}/voices/add`, {
+		const response = await fetch(`${ELEVENLABS_BASE_URL}/voices/add`, {
 			method: 'POST',
 			headers: ELEVENLABS_API_HEADERS,
 			body: formData,
@@ -19,18 +22,27 @@ export async function CreateVoiceClone(prevState: any, formData: FormData) {
 
 		const data = await response.json();
 		console.log('voice:', data.voice_id);
-		//todo: add voice_id to db    return { message:`${data.voice_id}` };
+		return {
+			status: 'success',
+			message: `Voice created`,
+			data: {voice_id: data.voice_id},
+		};
 	} catch (error) {
-		return {message: `Error: ${error}`};
+		console.error(error);
+		return {status: 'error', message: `Error: ${error}`};
 	}
 }
 
-export async function CreateAudioClip(prevState: any, formData: FormData) {
+export async function createElevenLabsTTSAction(
+	prevState: any,
+	formData: FormData
+): Promise<ActionStatus> {
 	const voice_id = formData.get('voice_id');
 
 	const userText = formData.get('text');
 	console.log('voice_id: ', voice_id);
 	console.log('text: ', userText);
+	//todo: add zod validation
 
 	const requestBody = JSON.stringify({
 		model_id: 'eleven_multilingual_v2',
@@ -38,30 +50,29 @@ export async function CreateAudioClip(prevState: any, formData: FormData) {
 	});
 
 	try {
-		const response = await fetch(`${BASE_URL}/text-to-speech/${voice_id}`, {
-			method: 'POST',
-			headers: {
-				...ELEVENLABS_API_HEADERS,
-				'Content-Type': 'application/json',
-			},
-			body: requestBody,
-		});
+		const response = await fetch(
+			`${ELEVENLABS_BASE_URL}/text-to-speech/${voice_id}`,
+			{
+				method: 'POST',
+				headers: {
+					...ELEVENLABS_API_HEADERS,
+					'Content-Type': 'application/json',
+				},
+				body: requestBody,
+			}
+		);
 		console.log('response: ', response);
 		if (response.ok) {
 			const blob = await response.blob();
 			const buffer = await blob.arrayBuffer();
-			const fileName = `${uuidv4()}.mp3`;
-			const filePath = `public/${fileName}`;
-			console.log('filePath: ', filePath);
-			fs.writeFileSync(filePath, Buffer.from(buffer));
-			console.log('** file writing complete** ');
-			//todo: add audio file to db/storage
 
-			//todo: redirect to `/text-to-speech/${audio_id}` or similiar
-			return {message: 'audio recording complete', audioUrl: `${fileName}`};
+			return {status: 'success', message: 'TTS Audio created', data: {buffer}};
 		}
+		const message =
+			response.status === 400 ? 'Error: Bad request' : 'Error: Unknown error';
+		return {status: 'error', message: message};
 	} catch (error) {
 		console.error(error);
-		return {message: `Error: ${error}`};
+		return {status: 'error', message: `Error: ${error}`};
 	}
 }
