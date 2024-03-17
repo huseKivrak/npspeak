e55524beddf144ed9cf6145b1c3ef18b53c9ac2e
Huse Kivrak <huse7@pm.me>
2024-03-16 23:51:09 -0400

    feature - improved functionality, props, etc.

--- components/ErrorToast.tsx
+++ components/ErrorToast.tsx
@@ -1,3 +1,19 @@
-export default function ErrorToast({message}: {message: string}) {
-	return <div className='text-xs text-red-600 ml-2'>{message}</div>;
+interface ErrorToastProps {
+	text: string;
+	className?: string;
+	children?: React.ReactNode;
+}
+
+export default function ErrorToast({
+	text,
+	className,
+	children,
+}: ErrorToastProps) {
+	const toastClasses = `text-error ml-2 ${className || ''}`;
+	return (
+		<div className={toastClasses}>
+			{text}
+			{children}
+		</div>
+	);
 }
