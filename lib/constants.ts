5e36fad90afa6f3ff5c1ad58db8a547278299a9d
Huse Kivrak <huse7@pm.me>
2024-03-18 18:22:14 -0400

    refactor / play - trying icon alternatives, other styling approaches

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,18 +1,45 @@
-import {
-	FaRegSmile,
-	FaRegSadCry,
-	FaBook,
-	FaQuestion,
-	FaRegCheckCircle,
-	FaExclamation,
-} from 'react-icons/fa';
-import {DialogueOptions} from '@/types/drizzle';
-export const DefaultDialogueOptions: DialogueOptions = [
-	{label: 'greeting', value: 1, Icon: FaRegSmile, color: 'text-primary'},
-	{label: 'farewell', value: 2, Icon: FaRegSadCry, color: 'text-error'},
-	{label: 'story', value: 3, Icon: FaBook, color: 'text-info'},
-	{label: 'other', value: 4, Icon: FaQuestion, color: 'text-secondary'},
-	{label: 'question', value: 5, Icon: FaQuestion, color: 'text-warning'},
-	{label: 'answer', value: 6, Icon: FaRegCheckCircle, color: 'text-accent'},
-	{label: 'exclamation', value: 7, Icon: FaExclamation, color: 'text-success'},
+import {DefaultDialogueStyles} from '@/types/drizzle';
+export const DefaultDialogueOptions: DefaultDialogueStyles[] = [
+	{
+		label: 'greeting',
+		value: 1,
+		iconName: 'FaRegSmile',
+		color: 'text-primary',
+	},
+	{
+		label: 'farewell',
+		value: 2,
+		iconName: 'FaRegSadCry',
+		color: 'text-error',
+	},
+	{
+		label: 'story',
+		value: 3,
+		iconName: 'FaBook',
+		color: 'text-info',
+	},
+	{
+		label: 'other',
+		value: 4,
+		iconName: 'FaQuestion',
+		color: 'text-secondary',
+	},
+	{
+		label: 'question',
+		value: 5,
+		iconName: 'FaQuestion',
+		color: 'text-warning',
+	},
+	{
+		label: 'answer',
+		value: 6,
+		iconName: 'FaRegCheckCircle',
+		color: 'text-accent',
+	},
+	{
+		label: 'exclamation',
+		value: 7,
+		iconName: 'FaExclamation',
+		color: 'text-success',
+	},
 ];
