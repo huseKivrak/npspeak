f5b7fdf4d3d6ba339d4c555a5a29ba5638b9e0d7
Huse Kivrak <huse7@pm.me>
2024-03-19 01:09:51 -0400

    refactor - listing all npcs with new render pattern

--- app/[username]/npcs/page.tsx
+++ app/[username]/npcs/page.tsx
@@ -1,17 +1,26 @@
 import {getNPCsWithRelatedData} from '@/database/drizzle/queries';
-import NPCCard from '@/components/cards/NPCCard';
+import NPCTabListCard from '@/components/cards/NPCTabListCard';
+import {createAllTabData} from '@/actions/npcTabData';
 
 export default async function NPCsPage({params}: {params: {username: string}}) {
 	const npcs = await getNPCsWithRelatedData();
 	const username = params.username;
+	if (!npcs) return <p>no npcs!</p>;
+
+	const totalNPCTabData = await Promise.all(
+		npcs.map(async (n) => {
+			const tabData = await createAllTabData(n);
+			return {npc: n, tabData};
+		})
+	);
 	return (
 		<div>
 			<h1>{username}&apos;s npcs</h1>
 			<ul>
-				{npcs &&
-					npcs.map((n) => (
-						<li key={n.id}>
-							<NPCCard npc={n} />
+				{totalNPCTabData &&
+					totalNPCTabData.map((n) => (
+						<li key={n.npc.id}>
+							<NPCTabListCard npc={n.npc} allTabData={n.tabData} />
 						</li>
 					))}
 			</ul>
