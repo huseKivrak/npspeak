c256cbefb355bf0c9ff0738f16334bcec70f34e9
Huse Kivrak <huse7@pm.me>
2024-03-21 15:42:52 -0400

    minor - removed old, unused functions, icon styling (error passing to client components)

--- utils/formatHelpers.ts
+++ utils/formatHelpers.ts
@@ -1,7 +1,6 @@
 import {DefaultDialogueOptions} from '@/lib/constants';
 import {DetailedNPC, FormOptions} from '@/types/drizzle';
 import {Tables} from '@/types/supabase';
-import {ElevenLabsVoice} from '@/types/elevenlabs';
 
 export const createStyledDialogue = (dialogue: Tables<'npc_dialogues'>) => {
 	const style = dialogue.dialogue_type_id
@@ -11,13 +10,11 @@ export const createStyledDialogue = (dialogue: Tables<'npc_dialogues'>) => {
 		: null;
 
 	const {id, text} = dialogue;
-	const icon = style?.icon || null;
 	const color = style?.color ? style.color : 'text-primary';
 
 	return {
 		id,
 		text,
-		icon,
 		color,
 	};
 };
@@ -48,42 +45,4 @@ export const createTTSFormOptions = (dialogues: Tables<'npc_dialogues'>[]) => {
 		}
 		return acc;
 	}, []);
-};
-
-const createStyledVoiceLabels = (voice: ElevenLabsVoice) => {
-	const gender = voice.labels.gender;
-	const age = voice.labels.age;
-	const useCase = voice.labels.use_case;
-
-	//todo: style these
-	const accent = voice.labels.accent;
-	const description = voice.labels.description;
-
-	const genderColor = gender === 'male' ? 'text-blue-500' : 'text-pink-500';
-	const ageColor =
-		age === 'young'
-			? 'text-green-200'
-			: age === 'middle aged'
-			? 'text-green-500'
-			: 'text-green-800';
-
-	return {
-		useCase,
-		genderColor,
-		ageColor,
-		accent,
-		description,
-	};
-};
-
-const getAllVoiceStyles = (voiceOptions: ElevenLabsVoice[]) => {
-	const styledVoices: Record<
-		string,
-		ReturnType<typeof createStyledVoiceLabels>
-	> = {};
-	voiceOptions.forEach((v) => {
-		styledVoices[v.voice_id] = createStyledVoiceLabels(v);
-	});
-	console.log('styledVoices', styledVoices);
-	return styledVoices;
 };
