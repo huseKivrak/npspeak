9de10911782a48cb7f01e271643964c124099848
Huse Kivrak <huse7@pm.me>
2024-03-21 15:41:26 -0400

    minor - removed unnecessary destructuring

--- components/icons/GenderIcon.tsx
+++ components/icons/GenderIcon.tsx
@@ -3,6 +3,5 @@ import {GenderIconMap} from '@/lib/constants';
 export const GenderIcon = ({gender}: {gender: string}) => {
 	const {icon: GenderIcon, props} =
 		GenderIconMap[gender] || GenderIconMap.default;
-	const {size, color} = props;
 	return <GenderIcon {...props} />;
 };
