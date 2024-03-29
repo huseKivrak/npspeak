3f6bf34f8ac95270a8ecba220dbc81659e448871
Huse Kivrak <huse7@pm.me>
2024-03-29 18:43:26 -0400

    feature / file - starting restructure of UI for streamlined table display, easy action buttons, etc. For now, roughly identical for campaigns, npcs, and npc dialogues.

---
+++ components/NPCDialogueTable.tsx
@@ -0,0 +1,104 @@
+import {getNPCDialogueTabData} from '@/actions/npcTabData';
+import {DetailedDialogue, DetailedNPC} from '@/types/drizzle';
+import AddDialogueToNPC from './cards/AddDialogueInTab';
+import Link from 'next/link';
+import {DialogueIcon} from './icons/DialogueIcon';
+import DeleteDialogueModal from './forms/DeleteDialogueModal';
+import AddTTSModal from './forms/AddTTSModal';
+import {
+	PiSkullBold,
+	PiMicrophoneBold,
+	PiMicrophoneSlashBold,
+} from 'react-icons/pi';
+
+export default async function NPCDialogueTable({npc}: {npc: DetailedNPC}) {
+	const dialogueData = await getNPCDialogueTabData(npc);
+	const detailedDialogues =
+		dialogueData.status === 'success' ? dialogueData.data : [];
+	return (
+		<div className=''>
+			<h1 className='text-3xl font-bold text-accent tracking-wide'>
+				{npc.npc_name}
+			</h1>
+			<p className='text-lg font-light text-primary'>{npc.description}</p>
+			<div className='divider' />
+			<h2 className='text-lg font-bold text-primary'>Campaigns:</h2>
+			{npc.campaigns && npc.campaigns.length > 0 ? (
+				<div className='flex flex-col gap-2'>
+					<ul>
+						{npc.campaigns.map((campaign) => (
+							<li key={campaign.id}>
+								<Link
+									href={`/campaigns/${campaign.id}`}
+									className='text-secondary'
+								>
+									{campaign.campaign_name}
+								</Link>
+							</li>
+						))}
+					</ul>
+				</div>
+			) : (
+				<p>No campaigns</p>
+			)}
+			<div className='divider' />
+
+			<div className='flex justify-start'>
+				<h2 className='mt-4 text-2xl font-bold text-primary mr-4'>Dialogue</h2>
+				<AddDialogueToNPC npcId={npc.id} />
+			</div>
+			<div className='overflow-x-auto'>
+				<table className='table-auto'>
+					<thead>
+						<tr>
+							<th>Type</th>
+							<th>Text</th>
+							<th>Audio</th>
+							<th>Actions</th>
+						</tr>
+					</thead>
+					<tbody>
+						{detailedDialogues.map((dialogue: DetailedDialogue) => {
+							return (
+								<tr key={dialogue.id}>
+									<td>
+										<div className='tooltip' data-tip={dialogue.dialogueType}>
+											<DialogueIcon dialogueType={dialogue.dialogueType} />
+										</div>
+									</td>
+									<td>{dialogue.text}</td>
+									<td>
+										{dialogue.audioURL ? (
+											<audio src={dialogue.audioURL} controls />
+										) : (
+											<PiMicrophoneSlashBold />
+										)}
+									</td>
+									<td>
+										<div className='flex gap-2'>
+											<DeleteDialogueModal
+												id={dialogue.id}
+												className='group btn btn-outline btn-error btn-xs hover:bg-error'
+											>
+												<PiSkullBold className='font-bold text-lg text-error group-hover:text-white' />
+											</DeleteDialogueModal>
+											{npc.voice_id && dialogue.audioURL === null && (
+												<AddTTSModal
+													dialogue={dialogue}
+													voiceId={npc.voice_id}
+													npcId={npc.id}
+												>
+													<PiMicrophoneBold className='font-bold text-lg text-primary group-hover:text-white' />
+												</AddTTSModal>
+											)}
+										</div>
+									</td>
+								</tr>
+							);
+						})}
+					</tbody>
+				</table>
+			</div>
+		</div>
+	);
+}
---
+++ components/NPCListTable.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import {DetailedNPC} from '@/types/drizzle';
+import Link from 'next/link';
+import DeleteNPCModal from './forms/DeleteNPCModal';
+import {PiSkullBold} from 'react-icons/pi';
+
+export default function NPCListTable({npcs}: {npcs: DetailedNPC[]}) {
+	console.log('npcs:', npcs);
+	return (
+		<div className='overflow-x-auto'>
+			<div className='flex justify-start items-center'>
+				<h2 className='text-2xl font-bold'>NPCs</h2>
+				<Link href={`/npcs/create`} className='btn btn-accent btn-xs ml-4'>
+					+ Create NPC
+				</Link>
+			</div>
+			<table className='table'>
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Description</th>
+						<th>Campaigns</th>
+						<th>Dialogue Count</th>
+						<th>Created</th>
+						<th>Actions</th>
+					</tr>
+				</thead>
+				<tbody>
+					{npcs.map((npc) => (
+						<tr key={npc.id} className='hover'>
+							<td className='text-primary link-hover font-bold'>
+								<Link href={`/npcs/${npc.id}`}>{npc.npc_name}</Link>
+							</td>
+							<td>{npc.description}</td>
+							<td className='flex flex-col gap-2 text-secondary link-hover font-medium'>
+								<ul>
+									{npc.campaigns.map((campaign) => (
+										<li key={campaign.id}>
+											<Link href={`/campaigns/${campaign.id}`}>
+												{campaign.campaign_name}
+											</Link>
+										</li>
+									))}
+								</ul>
+							</td>
+							<td>{npc.dialogues.length}</td>
+							<td>{npc.created_at.toString()}</td>
+							<td>
+								<DeleteNPCModal
+									id={npc.id}
+									className='group btn btn-outline btn-error btn-xs hover:bg-error'
+								>
+									<div className='tooltip tooltip-error' data-tip='delete'>
+										<PiSkullBold className='font-bold text-lg text-error group-hover:text-white' />
+									</div>
+								</DeleteNPCModal>
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
+}
