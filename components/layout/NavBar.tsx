f0bc0fe4d7b6f8ac38d18e6e40891e4f6c1dc81e
Huse Kivrak <huse7@pm.me>
2024-04-03 15:40:41 -0400

    feature - nextui navbar

--- components/layout/NavBar.tsx
+++ components/layout/NavBar.tsx
@@ -1,17 +1,28 @@
+'use client';
+import {
+	Navbar,
+	NavbarBrand,
+	NavbarContent,
+	NavbarItem,
+} from '@nextui-org/react';
 import AuthButton from '../buttons/AuthButton';
 import Link from 'next/link';
+import {ScrollEmoji} from '../icons/ScrollEmoji';
 
 export default function NavBar() {
 	return (
-		<div className='navbar bg-primary'>
-			<div className='navbar-start text-primary-content'>
-				<Link href='/' className='btn btn-ghost text-xl'>
-					npSpeak
+		<Navbar>
+			<NavbarBrand>
+				<Link href='/'>
+					<ScrollEmoji />
+					<p className='font-bold text-inherit'>npSpeak</p>
 				</Link>
-			</div>
-			<div className='navbar-end'>
-				<AuthButton />
-			</div>
-		</div>
+			</NavbarBrand>
+			<NavbarContent className='hidden sm:flex gap-4' justify='center'>
+				<NavbarItem>
+					<AuthButton />
+				</NavbarItem>
+			</NavbarContent>
+		</Navbar>
 	);
 }
