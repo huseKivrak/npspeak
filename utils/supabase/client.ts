2eb4c3b1df33b133962597b8806dbaa7a854fc63
Huse Kivrak <huse7@pm.me>
2024-04-04 14:20:50 -0400

    refactor - renamed for clarity

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,8 +1,8 @@
-import { createBrowserClient } from '@supabase/ssr';
+import {createBrowserClient} from '@supabase/ssr';
 
 //essentially createClient + Singleton pattern; avoids creating multiple clients
-export const createClient = () =>
-  createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
+export const createClientOnClient = () =>
+	createBrowserClient(
+		process.env.NEXT_PUBLIC_SUPABASE_URL!,
+		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+	);
