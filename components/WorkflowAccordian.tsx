ce21059357ab06be82557903945c32e28788f1b7
Huse Kivrak <huse7@pm.me>
2024-04-09 18:22:53 -0400

    refactor - improved styling; WIP

--- components/WorkflowAccordian.tsx
+++ components/WorkflowAccordian.tsx
@@ -6,47 +6,73 @@ import {
 	PiAddressBookBold,
 } from 'react-icons/pi';
 export const WorkflowAccordian = () => {
+	const itemClasses = {
+		base: 'py-0 w-full ',
+		title: 'font-normal text-medium',
+		trigger: 'px-2 py-0 rounded-lg h-14 flex items-center',
+		indicator: 'text-medium',
+		content: 'text-small px-2 text-start',
+	};
+
 	return (
-		<Accordion isCompact selectionMode='multiple' variant='splitted'>
+		<Accordion
+			keepContentMounted
+			selectionMode='multiple'
+			variant='splitted'
+			className='p-1 flex flex-col gap-1 w-full'
+			itemClasses={itemClasses}
+		>
 			<AccordionItem
 				key='1'
-				aria-label='1. Create NPC'
-				title='1. Create NPC'
-				indicator={<PiAddressBookBold size={24} className='text-primary-400' />}
+				aria-label='Create NPC'
+				title='Create NPC'
+				startContent={
+					<PiAddressBookBold size={24} className='text-primary-400' />
+				}
 				className='!bg-primary-50 tracking-widest'
 			>
-				<ul className='flex flex-col jusitfy-start items-start'>
-					<li>
-						<strong>Hassle-free character creation</strong>: Save the details
-						for your character sheet
-					</li>
-					<li>
-						<strong>Select a voice</strong>: Find the perfect voice from 50+
-						unique models
-					</li>
-				</ul>
+				<div className='flex flex-col space-y-2'>
+					<ul>
+						<li>
+							<strong>Hassle-free character creation</strong>: Save the details
+							for your character sheet
+						</li>
+						<li>
+							<strong>Select a voice</strong>: Find the perfect voice from 50+
+							unique models
+						</li>
+					</ul>
+				</div>
 			</AccordionItem>
 			<AccordionItem
 				key='2'
-				aria-label='2. Add Dialogue'
-				title='2. Add Dialogue'
-				indicator={<PiNotePencilBold size={24} className='text-primary-600' />}
+				aria-label='Add Dialogue'
+				title='Add Dialogue'
+				startContent={
+					<PiNotePencilBold size={24} className='text-primary-600' />
+				}
 				className='!bg-primary-100 tracking-widest'
 			>
-				Just select a dialogue type and add your text. Simple.
+				<div className=''>
+					<p>Just select the dialogue type and add your text. Simple.</p>
+				</div>
 			</AccordionItem>
 
 			<AccordionItem
 				key='3'
-				aria-label='3. Generate Audio'
-				title='3. Generate Audio'
-				indicator={<PiMicrophoneBold size={24} className='text-primary-700' />}
+				aria-label='Generate Audio'
+				title='Generate Audio'
+				startContent={
+					<PiMicrophoneBold size={24} className='text-primary-700' />
+				}
 				className='!bg-primary-200 tracking-widest'
 			>
-				<span style={{display: 'inline-flex', alignItems: 'center'}}>
-					Click the "<PiMicrophoneBold size={18} className='text-success' />"
-					next to each dialogue and hear your NPC come to life!
-				</span>
+				<div className=''>
+					<span style={{display: 'inline-flex', alignItems: 'center'}}>
+						Click the "<PiMicrophoneBold size={18} className='text-success' />"
+						next to each dialogue and hear your NPC come to life!
+					</span>
+				</div>
 			</AccordionItem>
 		</Accordion>
 	);
