54760911a81db487e32dcb97323925c5c8abaf87
Huse Kivrak <huse7@pm.me>
2024-04-10 12:30:59 -0400

    refactor -  moved auth page styling from auth forms to page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,10 @@
 import LoginForm from '@/components/forms/LoginForm';
 
 export default function LoginPage() {
-	return <LoginForm />;
+	return (
+		<div className='flex flex-col items-center justify-center gap-2'>
+			<h1 className='text-2xl'>login</h1>
+			<LoginForm />
+		</div>
+	);
 }
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,5 +1,10 @@
 import SignUpForm from '@/components/forms/SignUpForm';
 
 export default function SignUpPage() {
-  return <SignUpForm />;
+	return (
+		<div className='flex flex-col items-center gap-4'>
+			<h1 className='text-3xl text-primary'>sign up</h1>
+			<SignUpForm />
+		</div>
+	);
 }
