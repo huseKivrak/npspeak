f0fb3807b5ae3686bcb5c273034e8d946b77be91
Huse Kivrak <huse7@pm.me>
2024-04-10 12:37:02 -0400

    refactor - updated func declaration, removed outer div

--- components/forms/DialogueForm.tsx
+++ components/forms/DialogueForm.tsx
@@ -12,8 +12,9 @@ import {ErrorMessage} from '@hookform/error-message';
 import {ErrorToast} from '@/components/ErrorToast';
 import {DefaultDialogueTypes} from '@/lib/constants';
 import {RadioGroup, Radio, Textarea} from '@nextui-org/react';
+
 type Inputs = z.infer<typeof dialogueSchema>;
-export default function DialogueForm({npcId}: {npcId: number}) {
+export const DialogueForm = ({npcId}: {npcId: number}) => {
 	const [state, formAction] = useFormState<ActionStatus, FormData>(
 		createDialogueAction,
 		{status: 'idle', message: ''}
@@ -45,52 +46,56 @@ export default function DialogueForm({npcId}: {npcId: number}) {
 	}, [state, setError, reset]);
 
 	return (
-		<div className='flex flex-col items-start mt-4'>
-			<form className='flex flex-col gap-1 w-4/5 max-w-xs'>
-				<input type='hidden' {...register('npc_id')} value={npcId} />
-				<Controller
-					name='dialogue_type_id'
-					control={control}
-					defaultValue={DefaultDialogueTypes[0].value}
-					render={({field: {ref}}) => (
-						<RadioGroup
-							label='type'
-							orientation='horizontal'
-							name='dialogue_type_id'
-							isRequired
-							ref={ref}
-						>
-							{DefaultDialogueTypes.map((option) => (
-								<Radio key={option.label} value={option.value.toString()}>
-									{option.label}
-								</Radio>
-							))}
-						</RadioGroup>
-					)}
-				/>
-				<ErrorMessage errors={errors} name='dialogue_type_id' />
+		<form className='flex flex-col gap-1 w-4/5 max-w-xs items-start'>
+			<input type='hidden' {...register('npc_id')} value={npcId} />
+			<Controller
+				name='dialogue_type_id'
+				control={control}
+				defaultValue={DefaultDialogueTypes[0].value}
+				render={({field: {ref}}) => (
+					<RadioGroup
+						label='type'
+						orientation='horizontal'
+						name='dialogue_type_id'
+						size='sm'
+						isRequired
+						ref={ref}
+					>
+						{DefaultDialogueTypes.map((option) => (
+							<Radio key={option.label} value={option.value.toString()}>
+								{option.label}
+							</Radio>
+						))}
+					</RadioGroup>
+				)}
+			/>
+			<ErrorMessage errors={errors} name='dialogue_type_id' />
 
-				<Controller
-					name='text'
-					control={control}
-					render={({field}) => (
-						<Textarea
-							{...field}
-							label='text'
-							className='max-w-xs mt-4'
-							variant='bordered'
-							labelPlacement='outside'
-							isRequired
-						/>
-					)}
-				/>
-				<ErrorMessage errors={errors} name='text' />
+			<Controller
+				name='text'
+				control={control}
+				render={({field}) => (
+					<Textarea
+						{...field}
+						label='text'
+						className='max-w-xs mt-4'
+						variant='bordered'
+						labelPlacement='outside'
+						isRequired
+					/>
+				)}
+			/>
+			<ErrorMessage errors={errors} name='text' />
 
-				<SubmitButton formAction={formAction} pendingText='adding dialogue...'>
-					add dialogue
-				</SubmitButton>
-			</form>
+			<SubmitButton
+				variant='flat'
+				color='success'
+				formAction={formAction}
+				pendingText='adding dialogue...'
+			>
+				add dialogue
+			</SubmitButton>
 			{state?.status === 'error' && <ErrorToast text={state.message} />}
-		</div>
+		</form>
 	);
-}
+};
