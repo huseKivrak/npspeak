25b199a1acb1942d63583b9b3e290499e3a6af06
Huse Kivrak <huse7@pm.me>
2024-04-10 12:35:39 -0400

    minor - changed color

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,7 @@ import {Spinner} from '@nextui-org/spinner';
 export default function Loading() {
 	return (
 		<div className='flex items-center justify-center h-3/5'>
-			<Spinner size='lg' color='warning' />
+			<Spinner size='lg' color='primary' />
 		</div>
 	);
 }
