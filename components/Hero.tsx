2f74f3beab15a159bec4f54edb3b569f485ca8bf
Huse Kivrak <huse7@pm.me>
2024-04-11 18:33:20 -0400

    minor - styling, copy variants

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,11 +3,11 @@ import Link from 'next/link';
 
 export const Hero = () => {
 	return (
-		<div className='flex flex-col gap-2'>
+		<div className='flex flex-col gap-2 items-center justify-center'>
 			<h1 className='text-6xl lg:text-8xl font-bold text-secondary-500'>
 				npSpeak
 			</h1>
-			<p className='text-xl lg:text-2xl'>Bring your character's dialogue </p>
+			<p className='text-xl lg:text-2xl'>Give your character a voice</p>
 			<div className='flex gap-4 py-2'>
 				<Button variant='flat' color='success'>
 					<Link href='/signup'>Get started</Link>
