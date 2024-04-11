32ad30d245715ed8c629b4a4affdfcc539993587
Huse Kivrak <huse7@pm.me>
2024-04-11 14:29:51 -0400

    refactor - updated classes, added icon

--- components/ErrorToast.tsx
+++ components/ErrorToast.tsx
@@ -4,11 +4,16 @@ interface ErrorToastProps {
 	children?: React.ReactNode;
 }
 
+import {ErrorIcon} from './icons';
+
 export function ErrorToast({text, className, children}: ErrorToastProps) {
-	const toastClasses = `text-error ml-2 ${className || ''}`;
+	const toastClasses = `text-danger text-small ${className || ''}`;
 	return (
 		<div className={toastClasses}>
-			{text}
+			<div className='flex items-center'>
+				<ErrorIcon size={16} />
+				<span className='ml-1'>{text}</span>
+			</div>
 			{children}
 		</div>
 	);
