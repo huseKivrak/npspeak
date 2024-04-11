5deb21c26fac35a0f309d7dcafabbb3c39adb034
Huse Kivrak <huse7@pm.me>
2024-04-11 12:21:58 -0400

    refactor - styling, moving elements into child components, etc.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import {redirect} from 'next/navigation';
 import {getUserInfo} from '@/actions/auth';
-import SignUpForm from '@/components/forms/SignUpForm';
 import {WorkflowAccordian} from '@/components/WorkflowAccordian';
 import {VoiceSampler} from '@/components/VoiceSampler';
 import {getAllElevenLabsVoices} from '@/actions/elevenLabs';
-
+import {Hero} from '@/components/Hero';
+import {Divider} from '@nextui-org/divider';
 export default async function Index({
 	searchParams,
 }: {
@@ -20,27 +20,21 @@ export default async function Index({
 	const voiceSample = voices.slice(0, 12);
 
 	return (
-		<div className='flex flex-col items-center justify-center py-8 md:py-10'>
+		<div className='flex flex-col items-center justify-center text-center py-8 md:py-10'>
 			{searchParams?.message === 'logout' && (
 				<div className='bg-success text-white text-sm p-2 rounded-md w-fit mt-0 mb-2'>
 					You have been logged out.
 				</div>
 			)}
-			<div className='text-center '>
-				<h1 className='text-4xl lg:text-6xl font-bold text-secondary-500'>
-					npSpeak
-				</h1>
-				<p className='text-lg'>NPC Dialogue made simple.</p>
-
-				<h2 className='text-2xl my-4'>Character audio in 3 easy steps</h2>
-				<WorkflowAccordian />
-				<div className='flex flex-col items-start mt-8 space-y-2'>
-					{voices ? (
-						<VoiceSampler voices={voiceSample} />
-					) : (
-						<div>Voice Sampler Unavailable</div>
-					)}
-				</div>
+			<Hero />
+			<WorkflowAccordian />
+			<Divider className='my-4' />
+			<div className='flex flex-col items-start space-y-2'>
+				{voices ? (
+					<VoiceSampler voices={voiceSample} />
+				) : (
+					<div>Voice Sampler Unavailable</div>
+				)}
 			</div>
 		</div>
 	);
