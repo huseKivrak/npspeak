c21a278b03efede459beb8422c0f8b491162f8fb
Huse Kivrak <huse7@pm.me>
2024-04-11 14:43:40 -0400

    feature - added search icon

--- components/icons.tsx
+++ components/icons.tsx
@@ -364,3 +364,36 @@ export const SunFilledIcon = ({
 		</g>
 	</svg>
 );
+
+export const SearchIcon = ({
+	size = 24,
+	width,
+	height,
+	...props
+}: IconSvgProps) => (
+	<svg
+		aria-hidden='true'
+		fill='none'
+		focusable='false'
+		height={size || height}
+		width={size || width}
+		role='presentation'
+		viewBox='0 0 24 24'
+		{...props}
+	>
+		<path
+			d='M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z'
+			stroke='currentColor'
+			strokeLinecap='round'
+			strokeLinejoin='round'
+			strokeWidth='2'
+		/>
+		<path
+			d='M22 22L20 20'
+			stroke='currentColor'
+			strokeLinecap='round'
+			strokeLinejoin='round'
+			strokeWidth='2'
+		/>
+	</svg>
+);
