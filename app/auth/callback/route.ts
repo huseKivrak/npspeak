0c12a018a8417071ee7d385d12298db845ae41fa
Huse Kivrak <huse7@pm.me>
2024-04-14 01:24:31 -0400

    removed comment

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -13,6 +13,5 @@ export async function GET(request: Request) {
 		await supabase.auth.exchangeCodeForSession(code);
 	}
 
-	// URL to redirect to after sign in process completes
 	return NextResponse.redirect(`${origin}/dashboard`);
 }
