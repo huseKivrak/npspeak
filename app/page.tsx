81c9feb166be26f99bfaa23a69381e765b5df297
Huse Kivrak <huse7@pm.me>
2024-04-14 01:26:44 -0400

    styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import {redirect} from 'next/navigation';
 import {getUserInfo} from '@/actions/auth';
-import {WorkflowAccordian} from '@/components/WorkflowAccordian';
 import {VoiceSampler} from '@/components/VoiceSampler';
 import {getAllElevenLabsVoices} from '@/actions/elevenLabs';
+import {WorkflowAccordian} from '@/components/WorkflowAccordian';
 import {Hero} from '@/components/Hero';
-import {Divider} from '@nextui-org/divider';
+
 export default async function Index({
 	searchParams,
 }: {
@@ -20,22 +20,15 @@ export default async function Index({
 	const voiceSample = voices.slice(0, 12);
 
 	return (
-		<div className='flex flex-col items-center justify-center text-center py-8 md:py-10'>
+		<div className='flex flex-col items-center text-center gap-4'>
 			{searchParams?.message === 'logout' && (
-				<div className='bg-success text-white text-sm p-2 rounded-md w-fit mt-0 mb-2'>
+				<div className='bg-success text-white text-small p-2 rounded-small w-fit'>
 					You have been logged out.
 				</div>
 			)}
 			<Hero />
 			<WorkflowAccordian />
-			<Divider className='my-4' />
-			<div className='flex flex-col items-start space-y-2'>
-				{voices ? (
-					<VoiceSampler voices={voiceSample} />
-				) : (
-					<div>Voice Sampler Unavailable</div>
-				)}
-			</div>
+			{voices && <VoiceSampler voices={voiceSample} />}
 		</div>
 	);
 }
