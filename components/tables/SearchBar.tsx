9ceea29852b26be5dc57aea0720b0a8ce796082b
Huse Kivrak <huse7@pm.me>
2024-05-01 15:05:16 -0400

    refactor / file - table searchbar separated out

---
+++ components/tables/SearchBar.tsx
@@ -0,0 +1,24 @@
+import {Input} from '@nextui-org/input';
+
+export const SearchBar = ({
+	filterValue,
+	onClear,
+	onValueChange,
+	...props
+}: {
+	filterValue: string;
+	onClear: () => void;
+	onValueChange: (value: string) => void;
+}) => {
+	return (
+		<Input
+			isClearable
+			className='w-full sm:max-w-[44%]'
+			placeholder='Search by name...'
+			value={filterValue}
+			onValueChange={onValueChange}
+			onClear={onClear}
+			{...props}
+		/>
+	);
+};
