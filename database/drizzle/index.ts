8c978f675c5d99bc094202cc1c1f23929c49cb5d
Huse Kivrak <huse7@pm.me>
2024-05-02 13:43:07 -0400

    testing connection pooler in prod

--- database/drizzle/index.ts
+++ database/drizzle/index.ts
@@ -1,5 +1,9 @@
 import {drizzle} from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
+import {cwd} from 'process';
+import {loadEnvConfig} from '@next/env';
+
+loadEnvConfig(cwd());
 
 // const connectionString = process.env.SUPABASE_POOLING_STRING || '';
 const connectionString = process.env.DATABASE_URL || '';
