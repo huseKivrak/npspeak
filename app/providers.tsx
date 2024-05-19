54c76b69d22c379f58f764e8edcae47bac948bce
Huse Kivrak <huse7@pm.me>
2024-05-19 16:47:13 -0400

    minor - autoformat

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,10 +1,10 @@
 'use client';
-import {NextUIProvider} from '@nextui-org/system';
-import {useRouter} from 'next/navigation';
-import {ThemeProvider as NextThemesProvider} from 'next-themes';
-import {ThemeProviderProps} from 'next-themes/dist/types';
+import { NextUIProvider } from '@nextui-org/system';
+import { useRouter } from 'next/navigation';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { ThemeProviderProps } from 'next-themes/dist/types';
 
-export function Providers({children}: {children: React.ReactNode}) {
+export function Providers({ children }: { children: React.ReactNode }) {
 	const router = useRouter();
 
 	return (
