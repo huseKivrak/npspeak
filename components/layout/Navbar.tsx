efde5e7fdbcb3b1ee3fb3dd809d9416d4cd75650
Huse Kivrak <huse7@pm.me>
2024-05-30 19:39:04 -0400

    styling

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,29 +1,23 @@
 'use client';
-import {
-	Navbar as NextUINavbar,
-	NavbarBrand,
-	NavbarContent,
-} from '@nextui-org/navbar';
+import { Navbar as NextUINavbar, NavbarBrand, NavbarContent } from '@nextui-org/navbar';
 import Link from 'next/link';
-import {ScrollEmojiIcon} from '../icons';
-import {ThemeSwitcher} from '../buttons/ThemeSwitcher';
+import { ScrollEmojiIcon } from '../icons';
+import { ThemeSwitcher } from '../buttons/ThemeSwitcher';
 
-export const Navbar = ({children}: {children: React.ReactNode}) => {
+export const Navbar = ({ children }: { children: React.ReactNode }) => {
 	return (
-		<NextUINavbar shouldHideOnScroll maxWidth='lg' position='sticky'>
+		<NextUINavbar shouldHideOnScroll maxWidth='lg' position='sticky' className='mt-4'>
 			<NavbarContent justify='start'>
 				<NavbarBrand as='li' className='gap-3'>
 					<Link href='/' className='flex justify-start items-center'>
-						<ScrollEmojiIcon />
-						<p className='text-warning tracking-widest font-thin font-alagard'>
-							npSpeak
-						</p>
+						<ScrollEmojiIcon size={48} />
+						<p className='text-4xl text-warning tracking-widest font-thin font-alagard'>npSpeak</p>
 					</Link>
 				</NavbarBrand>
 			</NavbarContent>
 			<NavbarContent justify='end'>
 				{children}
-				<ThemeSwitcher />
+				{/* <ThemeSwitcher /> */}
 			</NavbarContent>
 		</NextUINavbar>
 	);
