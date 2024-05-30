cf5ccf5942ede191c8bbff59890f27e59a66ec2e
Huse Kivrak <huse7@pm.me>
2024-05-30 16:58:40 -0400

    feature - uses select instead of radio group

--- components/forms/DialogueForm.tsx
+++ components/forms/DialogueForm.tsx
@@ -1,29 +1,30 @@
 'use client';
-import {useEffect} from 'react';
-import {useForm, Controller, FieldPath} from 'react-hook-form';
-import {useFormState} from 'react-dom';
-import {z} from 'zod';
-import {zodResolver} from '@hookform/resolvers/zod';
-import {createDialogueAction} from '@/actions/db/dialogue';
-import {dialogueSchema} from '@/database/drizzle/validation';
-import {SubmitButton} from '@/components/buttons/SubmitButton';
-import {ActionStatus} from '@/types/drizzle';
-import {ErrorMessage} from '@hookform/error-message';
-import {ErrorToast} from '@/components/ErrorToast';
-import {DefaultDialogueTypes} from '@/lib/constants';
-import {RadioGroup, Radio, Textarea} from '@nextui-org/react';
+import { useEffect } from 'react';
+import { useForm, Controller, FieldPath } from 'react-hook-form';
+import { useFormState } from 'react-dom';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createDialogueAction } from '@/actions/db/dialogue';
+import { dialogueSchema } from '@/database/drizzle/validation';
+import { SubmitButton } from '@/components/buttons/SubmitButton';
+import { ActionStatus } from '@/types/drizzle';
+import { ErrorMessage } from '@hookform/error-message';
+import { ErrorToast } from '@/components/ErrorToast';
+import { DefaultDialogueTypes } from '@/lib/constants';
+import { RadioGroup, Radio, Textarea } from '@nextui-org/react';
+import { Select, SelectItem } from '@nextui-org/react';
 
 type Inputs = z.infer<typeof dialogueSchema>;
-export const DialogueForm = ({npcId}: {npcId: number}) => {
-	const [state, formAction] = useFormState<ActionStatus, FormData>(
-		createDialogueAction,
-		{status: 'idle', message: ''}
-	);
+export const DialogueForm = ({ npcId }: { npcId: number }) => {
+	const [state, formAction] = useFormState<ActionStatus, FormData>(createDialogueAction, {
+		status: 'idle',
+		message: '',
+	});
 
 	const {
 		register,
 		control,
-		formState: {errors},
+		formState: { errors },
 		setError,
 		reset,
 	} = useForm<Inputs>({
@@ -53,22 +54,23 @@ export const DialogueForm = ({npcId}: {npcId: number}) => {
 			<Controller
 				name='dialogue_type_id'
 				control={control}
-				defaultValue={DefaultDialogueTypes[0].value}
-				render={({field: {ref}}) => (
-					<RadioGroup
+				render={({ field }) => (
+					<Select
+						{...field}
 						label='Dialogue Type'
-						orientation='horizontal'
-						name='dialogue_type_id'
-						size='sm'
+						className='max-w-xs mt-4'
+						variant='bordered'
+						labelPlacement='outside'
 						isRequired
-						ref={ref}
+						{...register('dialogue_type_id')}
+						items={DefaultDialogueTypes}
 					>
-						{DefaultDialogueTypes.map((option) => (
-							<Radio key={option.label} value={option.value.toString()}>
-								{option.label}
-							</Radio>
-						))}
-					</RadioGroup>
+						{(item) => (
+							<SelectItem key={item.value} value={item.value}>
+								{item.label}
+							</SelectItem>
+						)}
+					</Select>
 				)}
 			/>
 			<ErrorMessage errors={errors} name='dialogue_type_id' />
@@ -76,7 +78,7 @@ export const DialogueForm = ({npcId}: {npcId: number}) => {
 			<Controller
 				name='text'
 				control={control}
-				render={({field}) => (
+				render={({ field }) => (
 					<Textarea
 						{...field}
 						label='Text'
