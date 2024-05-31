8687005338b36e4083c0e8db6e7209c97bc089c8
Huse Kivrak <huse7@pm.me>
2024-05-31 02:23:39 -0400

    styling

--- components/NPCDetail.tsx
+++ components/NPCDetail.tsx
@@ -24,24 +24,24 @@ export const NPCDetail = ({
 	console.log('soundboardDialogues:', soundboardDialogues);
 	return (
 		<div className='flex flex-col w-full'>
-			<div className='flex gap-2 w-5/6'>
+			<div className='flex flex-col gap-2 w-5/6'>
 				<h1 className='text-4xl lg:text-6xl text-primary'>{npc.npc_name}</h1>
 				<Divider orientation='vertical' className='mx-4' />
-				<p className='lg:text-xl'>{npc.description}</p>
+				<p className='lg:text-xl max-w-md'>{npc.description}</p>
 			</div>
 			<Divider className='my-4' />
 			<Soundboard dialogues={soundboardDialogues} />
 			<Divider className='my-4' />
 			<div className='flex flex-col gap-4'>
 				<div className='flex gap-4'>
-					<h2 className='text-xl'>{`${npc.npc_name}'s Dialogue`}</h2>
+					<h2 className='text-2xl'>All Dialogue</h2>
 					<Button
 						variant='flat'
 						color='success'
 						size='sm'
 						onClick={() => setShowDialogueForm(!showDialogueForm)}
 						startContent={showDialogueForm ? '' : <PlusIcon />}
-						className='w-fit'
+						className='w-fit text-white hover:text-white/50'
 					>
 						{showDialogueForm ? 'cancel' : 'dialogue'}
 					</Button>
