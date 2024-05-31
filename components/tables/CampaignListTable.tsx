2efe6df836649335965b1421fd9d521977e2d9d9
Huse Kivrak <huse7@pm.me>
2024-05-31 02:27:23 -0400

    styling / prettier

--- components/tables/CampaignListTable.tsx
+++ components/tables/CampaignListTable.tsx
@@ -1,6 +1,6 @@
 'use client';
-import {useCallback, useMemo, useState} from 'react';
-import {CampaignWithNPCs} from '@/types/drizzle';
+import { useCallback, useMemo, useState } from 'react';
+import { CampaignWithNPCs } from '@/types/drizzle';
 import {
 	Table,
 	TableHeader,
@@ -13,18 +13,14 @@ import {
 	Pagination,
 	SortDescriptor,
 } from '@nextui-org/react';
-import {deleteCampaignAction} from '@/actions/db/campaigns';
-import {DeleteIcon} from '../icons';
+import { deleteCampaignAction } from '@/actions/db/campaigns';
+import { DeleteIcon } from '../icons';
 import Link from 'next/link';
-import {DeleteModal} from '../DeleteModal';
-import {SearchBar} from './SearchBar';
-import {truncateText} from '@/utils/helpers/formHelpers';
+import { DeleteModal } from '../DeleteModal';
+import { SearchBar } from './SearchBar';
+import { truncateText } from '@/utils/helpers/formHelpers';
 
-export function CampaignListTable({
-	campaigns,
-}: {
-	campaigns: CampaignWithNPCs[];
-}) {
+export function CampaignListTable({ campaigns }: { campaigns: CampaignWithNPCs[] }) {
 	const [filterValue, setFilterValue] = useState('');
 	const [sortDescriptor, setSortDescriptor] = useState<SortDescriptor>({
 		column: 'campaign_name',
@@ -57,12 +53,8 @@ export function CampaignListTable({
 
 	const sortedItems = useMemo(() => {
 		return [...items].sort((a: CampaignWithNPCs, b: CampaignWithNPCs) => {
-			const first = a[
-				sortDescriptor.column as keyof CampaignWithNPCs
-			] as number;
-			const second = b[
-				sortDescriptor.column as keyof CampaignWithNPCs
-			] as number;
+			const first = a[sortDescriptor.column as keyof CampaignWithNPCs] as number;
+			const second = b[sortDescriptor.column as keyof CampaignWithNPCs] as number;
 			const cmp = first < second ? -1 : first > second ? 1 : 0;
 
 			return sortDescriptor.direction === 'descending' ? -cmp : cmp;
@@ -83,98 +75,74 @@ export function CampaignListTable({
 		setPage(1);
 	}, []);
 
-	const renderCell = useCallback(
-		(campaign: CampaignWithNPCs, columnKey: React.Key) => {
-			switch (columnKey) {
-				case 'campaign_name':
-					return (
-						<div className='flex flex-col'>
-							<p className='capitalize hover:underline'>
-								<Link href={`/campaigns/${campaign.id}`}>
-									{campaign.campaign_name}
-								</Link>
-							</p>
-						</div>
-					);
-				case 'description':
-					return (
-						<div className='flex flex-col'>
-							<Tooltip
-								delay={500}
-								closeDelay={0}
-								content={campaign.description}
-								className='max-w-xs'
-							>
-								<p className='text-tiny capitalize truncate max-w-xs h-8'>
-									{campaign.description}
-								</p>
+	const renderCell = useCallback((campaign: CampaignWithNPCs, columnKey: React.Key) => {
+		switch (columnKey) {
+			case 'campaign_name':
+				return (
+					<div className='flex flex-col'>
+						<p className='capitalize hover:underline'>
+							<Link href={`/campaigns/${campaign.id}`}>{campaign.campaign_name}</Link>
+						</p>
+					</div>
+				);
+			case 'description':
+				return (
+					<div className='flex flex-col'>
+						<Tooltip delay={500} closeDelay={0} content={campaign.description} className='max-w-xs'>
+							<p className='text-tiny capitalize truncate max-w-xs h-8'>{campaign.description}</p>
+						</Tooltip>
+					</div>
+				);
+			case 'npcs':
+				return (
+					<div className='flex flex-col gap-1'>
+						{campaign.npcs.length > 0 ? (
+							campaign.npcs.map((npc) => (
+								<Chip
+									key={npc.id}
+									size='sm'
+									color='success'
+									variant='flat'
+									className='hover:underline'
+								>
+									<Link key={npc.id} href={`/npcs/${npc.id}`} className='text-tiny'>
+										{truncateText(npc.npc_name ?? 'No NPCs.', 15)}
+									</Link>
+								</Chip>
+							))
+						) : (
+							<p className='text-tiny'> No NPCs</p>
+						)}
+					</div>
+				);
+			case 'created_at':
+				return (
+					<div className='flex flex-col'>
+						<p className='text-tiny capitalize '>
+							{new Date(campaign.created_at).toLocaleDateString()}
+						</p>
+					</div>
+				);
+			case 'actions':
+				return (
+					<div className='relative flex justify-center gap-2'>
+						<DeleteModal idName='campaign_id' serverAction={deleteCampaignAction} id={campaign.id}>
+							<Tooltip color='danger' content='Delete NPC'>
+								<DeleteIcon />
 							</Tooltip>
-						</div>
-					);
-				case 'npcs':
-					return (
-						<div className='flex flex-col gap-1'>
-							{campaign.npcs.length > 0 ? (
-								campaign.npcs.map((npc) => (
-									<Chip
-										key={npc.id}
-										size='sm'
-										color='warning'
-										variant='flat'
-										className='hover:underline'
-									>
-										<Link
-											key={npc.id}
-											href={`/npcs/${npc.id}`}
-											className='text-tiny'
-										>
-											{truncateText(npc.npc_name ?? 'No NPCs.', 15)}
-										</Link>
-									</Chip>
-								))
-							) : (
-								<p className='text-tiny'> No NPCs</p>
-							)}
-						</div>
-					);
-				case 'created_at':
-					return (
-						<div className='flex flex-col'>
-							<p className='text-tiny capitalize '>
-								{new Date(campaign.created_at).toLocaleDateString()}
-							</p>
-						</div>
-					);
-				case 'actions':
-					return (
-						<div className='relative flex justify-center gap-2'>
-							<DeleteModal
-								idName='campaign_id'
-								serverAction={deleteCampaignAction}
-								id={campaign.id}
-							>
-								<Tooltip color='danger' content='Delete NPC'>
-									<DeleteIcon />
-								</Tooltip>
-							</DeleteModal>
-						</div>
-					);
-				default:
-					return null;
-			}
-		},
-		[]
-	);
+						</DeleteModal>
+					</div>
+				);
+			default:
+				return null;
+		}
+	}, []);
 
 	//Search bar
 	const topContent = useMemo(() => {
 		return (
 			<div className='flex flex-col gap-2'>
-				<SearchBar
-					filterValue={filterValue}
-					onValueChange={onSearchChange}
-					onClear={onClear}
-				/>
+				<SearchBar filterValue={filterValue} onValueChange={onSearchChange} onClear={onClear} />
 			</div>
 		);
 	}, [filterValue, onSearchChange, onClear]);
@@ -222,12 +190,7 @@ export function CampaignListTable({
 			onSortChange={setSortDescriptor}
 		>
 			<TableHeader className=''>
-				<TableColumn
-					allowsSorting
-					align='center'
-					key='campaign_name'
-					maxWidth={48}
-				>
+				<TableColumn allowsSorting align='center' key='campaign_name' maxWidth={48}>
 					Name
 				</TableColumn>
 				<TableColumn key='description' align='center' maxWidth={48}>
@@ -244,9 +207,7 @@ export function CampaignListTable({
 			<TableBody items={sortedItems} emptyContent={'No campaigns to display.'}>
 				{(item) => (
 					<TableRow key={item.id}>
-						{(columnKey) => (
-							<TableCell>{renderCell(item, columnKey)}</TableCell>
-						)}
+						{(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
 					</TableRow>
 				)}
 			</TableBody>
