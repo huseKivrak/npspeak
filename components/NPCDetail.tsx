9f0065c6ce0459d1c2ef6d856d7b9f3842f5efba
Huse Kivrak <huse7@pm.me>
2024-06-09 23:34:36 -0400

    refactor - using tabs for dialogue displays

--- components/NPCDetail.tsx
+++ components/NPCDetail.tsx
@@ -5,12 +5,9 @@ import { DialogueListTable } from '@/components/tables/DialogueListTable'
 import { DialogueForm } from './forms/DialogueForm'
 import { Divider } from '@nextui-org/divider'
 import { Button } from '@nextui-org/button'
+import { Tabs, Tab } from '@nextui-org/tabs'
 import { PlusIcon } from './icons'
-import {
-  DetailedDialogue,
-  DetailedNPC,
-  SoundboardDialogue,
-} from '@/types/drizzle'
+import { DetailedDialogue, DetailedNPC } from '@/types/drizzle'
 import { formatDialoguesForSoundboard } from '@/utils/formatHelpers'
 import { Soundboard } from './soundboard/Soundboard'
 export const NPCDetail = ({
@@ -20,42 +17,48 @@ export const NPCDetail = ({
   npc: DetailedNPC
   dialogues?: DetailedDialogue[]
 }) => {
-  const [showDialogueForm, setShowDialogueForm] = useState(false)
-
-  const soundboardDialogues = dialogues
-    ? formatDialoguesForSoundboard(dialogues)
-    : []
-
-  console.log('dialogues:', dialogues)
-  console.log('soundboardDialogues:', soundboardDialogues)
   return (
     <div className="flex flex-col w-full">
-      <div className="flex flex-col gap-2 w-5/6">
+      <div className="flex flex-col gap-2 items-center">
         <h1 className="text-4xl lg:text-6xl text-primary">{npc.npc_name}</h1>
-        <Divider orientation="vertical" className="mx-4" />
-        <p className="lg:text-xl max-w-md">{npc.description}</p>
+        <p className="lg:text-xl">{npc.description}</p>
       </div>
+
       <Divider className="my-4" />
-      <Soundboard dialogues={soundboardDialogues} />
-      <Divider className="my-4" />
-      <div className="flex flex-col gap-4">
-        <div className="flex gap-4">
-          <h2 className="text-2xl">All Dialogue</h2>
-          <Button
-            variant="flat"
-            size="sm"
-            onClick={() => setShowDialogueForm(!showDialogueForm)}
-            startContent={showDialogueForm ? '' : <PlusIcon />}
-            className="w-fit "
-          >
-            {showDialogueForm ? 'cancel' : 'dialogue'}
-          </Button>
-        </div>
-        {showDialogueForm && <DialogueForm npcId={npc.id} />}
-        {dialogues && (
-          <DialogueListTable dialogues={dialogues} voiceId={npc.voice_id!} />
-        )}
-      </div>
+
+      <Tabs
+        isVertical
+        aria-label="Dialogue Views"
+        size="lg"
+        radius="sm"
+        color="secondary"
+      >
+        <Tab key="table" title="Table">
+          {dialogues ? (
+            <DialogueListTable dialogues={dialogues} voiceId={npc.voice_id!} />
+          ) : (
+            ''
+          )}
+        </Tab>
+        <Tab key="soundboard" title="Soundboard">
+          {dialogues ? (
+            <Soundboard dialogues={formatDialoguesForSoundboard(dialogues)} />
+          ) : (
+            ''
+          )}
+        </Tab>
+        <Tab
+          key="dialogue form"
+          title={
+            <div className="flex items-center space-x-2">
+              <PlusIcon />
+              <span>dialogue</span>
+            </div>
+          }
+        >
+          <DialogueForm npcId={npc.id} />
+        </Tab>
+      </Tabs>
     </div>
   )
 }
