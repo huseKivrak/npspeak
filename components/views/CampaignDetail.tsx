5ce68206a0f830e103c87fce8a518b6dfe40e5ca
Huse Kivrak <huse7@pm.me>
2024-06-10 01:16:16 -0400

    refactor - WIP

--- components/views/CampaignDetail.tsx
+++ components/views/CampaignDetail.tsx
@@ -1,10 +1,17 @@
 import { CampaignWithNPCs } from '@/types/drizzle';
 
-export function CampaignDetail({ campaign }: { campaign: CampaignWithNPCs }) {
+export const CampaignDetail = ({
+  campaign,
+}: {
+  campaign: CampaignWithNPCs;
+}) => {
   return (
-    <div>
-      <h1>{campaign.campaign_name}</h1>
-      <p>{campaign.description}</p>
+    <div className="flex flex-col w-full">
+      <div className="flex flex-col gap-2">
+        <h1>{campaign.campaign_name}</h1>
+
+        <p>{campaign.description}</p>
+      </div>
     </div>
   );
-}
+};
