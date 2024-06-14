6e0c8673bac01bf270e1c8bb3f1b6837c5e35a98
Huse Kivrak <huse7@pm.me>
2024-06-14 16:34:33 -0400

    feature / refactor - implemented voice filter component for voice select; WIP

--- components/forms/NPCForm.tsx
+++ components/forms/NPCForm.tsx
@@ -5,7 +5,14 @@ import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { createNPCAction, updateNPCAction } from '@/actions/db/NPCs';
 import { npcSchema } from '@/database/drizzle/validation';
-import { CheckboxGroup, Checkbox, Textarea, Button } from '@nextui-org/react';
+import {
+  CheckboxGroup,
+  Checkbox,
+  Textarea,
+  Button,
+  Spacer,
+  Input,
+} from '@nextui-org/react';
 import { SubmitButton } from '@/components/buttons/SubmitButton';
 import ReactSelect from 'react-select';
 import { VoiceOption } from './customSelect/VoiceOption';
@@ -14,8 +21,8 @@ import { PlusIcon } from '../icons';
 import { ErrorMessage } from '@hookform/error-message';
 import { ErrorToast } from '@/components/ErrorToast';
 import { FormOptions, UpdateNPC } from '@/types/drizzle';
-import { FormInput } from './FormInput';
-import { VoiceOptionProps } from '@/utils/helpers/formHelpers';
+import { VoiceFilterForm } from './customSelect/VoiceLabelFilter';
+import { VoiceOptionProps } from '@/types/elevenlabs';
 
 type Inputs = z.infer<typeof npcSchema>;
 
@@ -55,6 +62,10 @@ export const NPCForm = ({
 
   const hasCampaigns = campaignOptions && campaignOptions.length > 0;
 
+  const handleFilteredOptions = (filteredOptions: VoiceOptionProps[]) => {
+    setFilteredOptions(filteredOptions);
+  };
+
   const onSubmit = async (data: Inputs) => {
     console.log('Submitting');
     const form = document.getElementById('npc-form') as HTMLFormElement;
@@ -90,130 +101,135 @@ export const NPCForm = ({
   };
 
   return (
-    <form
-      id="npc-form"
-      onSubmit={handleSubmit(onSubmit, onInvalid)}
-      className="flex flex-col gap-4 w-full max-w-md"
-    >
-      {errors.root?.serverError && (
-        <ErrorToast text={errors.root.serverError.message!} />
-      )}
-
-      <FormInput
-        isRequired
-        {...register('npc_name')}
-        name="npc_name"
-        label="Name"
-        defaultValue={npcToUpdate?.npc_name}
-        placeholder="what are they called?"
-        variant="bordered"
+    <div className="flex w-full">
+      <VoiceFilterForm
+        voiceOptions={voiceOptions}
+        onFilterChange={handleFilteredOptions}
       />
-      <ErrorMessage
-        errors={errors}
-        name="npc_name"
-        render={({ message }) => <ErrorToast text={message} />}
-      />
-      <Textarea
-        {...register('description')}
-        name="description"
-        label="Description (optional)"
-        defaultValue={npcToUpdate?.description}
-        placeholder="describe your NPC"
-        variant="bordered"
-        className=" text-2xl"
-      />
-      <ErrorMessage
-        errors={errors}
-        name="description"
-        render={({ message }) => <ErrorToast text={message} />}
-      />
-      <Controller
-        name="voice_id"
-        control={control}
-        rules={{ required: true }}
-        render={({ field: { name, ref, onChange } }) => (
-          <ReactSelect
-            name={name}
-            ref={ref}
-            onChange={(value) => {
-              onChange(value?.value);
-              setSelectedVoiceURL(value?.sampleURL ?? '');
-            }}
-            components={{
-              Option: VoiceOption,
-              SingleValue: VoiceSingleValue,
-            }}
-            placeholder="Select a voice"
-            isSearchable={false}
-            options={filteredOptions}
-            defaultValue={
-              isEditing
-                ? filteredOptions.find(
-                    (option) => npcToUpdate?.voice_id === option.value
-                  )
-                : null
-            }
-          />
+      <Spacer x={12} />
+      <form
+        id="npc-form"
+        onSubmit={handleSubmit(onSubmit, onInvalid)}
+        className="flex flex-col gap-8 w-full max-w-lg border rounded-3xl p-4"
+      >
+        {errors.root?.serverError && (
+          <ErrorToast text={errors.root.serverError.message!} />
         )}
-      />
 
-      <div className="flex items-center my-4 gap-6">
-        <span className="text-secondary font-semibold">Voice Preview:</span>
-        <audio src={selectedVoiceURL ?? ''} controls autoPlay={autoplay} />
-        <Checkbox
-          isSelected={autoplay}
-          onValueChange={setAutoplay}
-          color="success"
-          radius="full"
-          size="lg"
-        >
-          Autoplay
-        </Checkbox>
-      </div>
-      {hasCampaigns && (
-        <Button
-          onClick={() => setShowAddCampaign(!showAddCampaign)}
-          variant="flat"
-          color="primary"
-          startContent={showAddCampaign ? '' : <PlusIcon />}
-        >
-          {showAddCampaign ? 'cancel' : 'campaign(s)'}
-        </Button>
-      )}
-      {showAddCampaign && hasCampaigns && (
+        <Input
+          isRequired
+          {...register('npc_name')}
+          name="npc_name"
+          label="Name"
+          defaultValue={npcToUpdate?.npc_name}
+          placeholder="what are they called?"
+          variant="bordered"
+        />
+        <ErrorMessage
+          errors={errors}
+          name="npc_name"
+          render={({ message }) => <ErrorToast text={message} />}
+        />
+        <Textarea
+          {...register('description')}
+          name="description"
+          label="Description (optional)"
+          defaultValue={npcToUpdate?.description}
+          placeholder="describe your NPC"
+          variant="bordered"
+          className=" text-2xl"
+          minRows={4}
+        />
+        <ErrorMessage
+          errors={errors}
+          name="description"
+          render={({ message }) => <ErrorToast text={message} />}
+        />
+
         <Controller
-          name="campaign_ids"
+          name="voice_id"
           control={control}
-          render={({ field: { ref } }) => (
-            <CheckboxGroup
-              label="campaigns"
-              name="campaign_ids"
-              orientation="horizontal"
+          rules={{ required: true }}
+          render={({ field: { name, ref, onChange } }) => (
+            <ReactSelect
+              name={name}
               ref={ref}
-            >
-              {campaignOptions.map((option) => (
-                <Checkbox
-                  key={option.label}
-                  value={option.value.toString()}
-                  isSelected={npcToUpdate?.campaign_ids?.includes(option.value)}
-                >
-                  {option.label}
-                </Checkbox>
-              ))}
-            </CheckboxGroup>
+              onChange={(value) => {
+                onChange(value?.value);
+                setSelectedVoiceURL(value?.sampleURL ?? '');
+              }}
+              components={{
+                Option: VoiceOption,
+                SingleValue: VoiceSingleValue,
+              }}
+              placeholder="Select a voice"
+              isSearchable={false}
+              options={filteredOptions}
+              defaultValue={null}
+            />
           )}
         />
-      )}
 
-      <SubmitButton
-        id="npc-form-submit"
-        pendingText={isEditing ? 'updating NPC...' : 'creating NPC...'}
-        variant="flat"
-        color="success"
-        className="mt-2 font-bold text-large"
-      >
-        {isEditing ? 'update' : 'create'}
-      </SubmitButton>
-    </form>
+        <div className="flex items-center my-4 gap-6">
+          <span className="text-secondary font-semibold">Voice Preview:</span>
+          <audio src={selectedVoiceURL ?? ''} controls autoPlay={autoplay} />
+          <Checkbox
+            isSelected={autoplay}
+            onValueChange={setAutoplay}
+            color="success"
+            radius="full"
+            size="lg"
+          >
+            Autoplay
+          </Checkbox>
+        </div>
+        {hasCampaigns && (
+          <Button
+            onClick={() => setShowAddCampaign(!showAddCampaign)}
+            variant="flat"
+            color="primary"
+            startContent={showAddCampaign ? '' : <PlusIcon />}
+          >
+            {showAddCampaign ? 'cancel' : 'campaign(s)'}
+          </Button>
+        )}
+        {showAddCampaign && hasCampaigns && (
+          <Controller
+            name="campaign_ids"
+            control={control}
+            render={({ field: { ref } }) => (
+              <CheckboxGroup
+                label="campaigns"
+                name="campaign_ids"
+                orientation="horizontal"
+                ref={ref}
+              >
+                {campaignOptions.map((option) => (
+                  <Checkbox
+                    key={option.label}
+                    value={option.value.toString()}
+                    isSelected={npcToUpdate?.campaign_ids?.includes(
+                      option.value
+                    )}
+                  >
+                    {option.label}
+                  </Checkbox>
+                ))}
+              </CheckboxGroup>
+            )}
+          />
+        )}
+
+        <SubmitButton
+          id="npc-form-submit"
+          pendingText={isEditing ? 'updating NPC...' : 'creating NPC...'}
+          variant="flat"
+          color="success"
+          className="mt-2 font-bold text-large"
+        >
+          {isEditing ? 'update' : 'create'}
+        </SubmitButton>
+      </form>
+    </div>
   );
 };
---
+++ components/forms/customSelect/VoiceLabelFilter.tsx
@@ -0,0 +1,219 @@
+'use client';
+import { useEffect, useMemo, useState } from 'react';
+import { getAllVoiceLabelOptions } from '@/utils/elevenlabs/api';
+import {
+  Button,
+  Checkbox,
+  CheckboxGroup,
+  Divider,
+  Tooltip,
+} from '@nextui-org/react';
+import {
+  getAccentEmoji,
+  voiceMatchesFilter,
+  VoiceOptionProps,
+} from '@/utils/helpers/formHelpers';
+import { capitalize } from '@/utils/formatHelpers';
+import { FaMale, FaFemale } from 'react-icons/fa';
+import { MdOutlineDirectionsWalk, MdOutlineElderly } from 'react-icons/md';
+import { BiChild } from 'react-icons/bi';
+import { VscRefresh } from 'react-icons/vsc';
+
+interface FilterState {
+  selectedGender: string[];
+  selectedAge: string[];
+  selectedAccent: string[];
+  selectedUseCase: string[];
+  selectedDescription: string[];
+}
+
+export function VoiceFilterForm({
+  voiceOptions,
+  onFilterChange,
+}: {
+  voiceOptions: VoiceOptionProps[];
+  onFilterChange: (filteredOptions: VoiceOptionProps[]) => void;
+}) {
+  const { gender, age, accent, useCase, description } =
+    getAllVoiceLabelOptions(voiceOptions);
+
+  const [filters, setFilters] = useState<FilterState>({
+    selectedGender: [],
+    selectedAge: [],
+    selectedAccent: [],
+    selectedUseCase: [],
+    selectedDescription: [],
+  });
+  console.log('voiceOptions:', voiceOptions);
+
+  const handleFilterChange = (
+    filterName: keyof FilterState,
+    value: string[]
+  ) => {
+    setFilters((prevFilters) => ({ ...prevFilters, [filterName]: value }));
+  };
+
+  const filteredOptions = useMemo(() => {
+    return voiceOptions.filter((voice) => {
+      return (
+        voiceMatchesFilter(filters.selectedGender, voice.gender) &&
+        voiceMatchesFilter(filters.selectedAge, voice.age) &&
+        voiceMatchesFilter(filters.selectedAccent, voice.accent) &&
+        voiceMatchesFilter(filters.selectedUseCase, voice.useCase) &&
+        voiceMatchesFilter(filters.selectedDescription, voice.description)
+      );
+    });
+  }, [voiceOptions, filters]);
+
+  const onReset = () => {
+    setFilters({
+      selectedGender: [],
+      selectedAge: [],
+      selectedAccent: [],
+      selectedUseCase: [],
+      selectedDescription: [],
+    });
+  };
+
+  useEffect(() => {
+    console.log('filteredOptions:', filters);
+    onFilterChange(filteredOptions);
+  }, [filteredOptions, onFilterChange]);
+
+  return (
+    <div className="flex flex-col max-w-md border rounded-3xl border-gray-200 p-4">
+      <Button
+        isIconOnly
+        type="reset"
+        onClick={onReset}
+        size="sm"
+        radius="full"
+        variant="flat"
+        className="absolute w-1 self-end h-min p-1"
+      >
+        <VscRefresh size={20} />
+      </Button>
+
+      <div className="flex space-x-8">
+        <CheckboxGroup
+          label="Gender"
+          onChange={(values) => handleFilterChange('selectedGender', values)}
+          orientation="horizontal"
+          size="sm"
+          radius="full"
+          value={filters.selectedGender}
+        >
+          <Checkbox key={'male'} value={'male'}>
+            <Tooltip content="male">
+              <span>
+                <FaMale size={30} color="lightblue" />
+              </span>
+            </Tooltip>
+          </Checkbox>
+          <Checkbox key={'female'} value={'female'}>
+            <Tooltip content="female">
+              <span>
+                <FaFemale size={30} color="lightpink" />
+              </span>
+            </Tooltip>
+          </Checkbox>
+        </CheckboxGroup>
+
+        <Divider orientation="vertical" className="h-14 mt-2 self-center" />
+
+        <CheckboxGroup
+          label="Age"
+          onChange={(values) => handleFilterChange('selectedAge', values)}
+          orientation="horizontal"
+          size="sm"
+          radius="full"
+          value={filters.selectedAge}
+        >
+          <Checkbox key={'young'} value={'young'}>
+            <span>
+              <Tooltip content="young">
+                <span>
+                  <BiChild size={32} className="text-green-200" />
+                </span>
+              </Tooltip>
+            </span>
+          </Checkbox>
+          <Checkbox key={'middle-aged'} value={'middle-aged'}>
+            <span>
+              <Tooltip content="middle-aged">
+                <span>
+                  <MdOutlineDirectionsWalk
+                    size={30}
+                    className="text-green-400"
+                  />
+                </span>
+              </Tooltip>
+            </span>
+          </Checkbox>
+          <Checkbox key={'old'} value={'old'}>
+            <span>
+              <Tooltip content="old">
+                <span>
+                  <MdOutlineElderly size={30} className="text-green-600" />
+                </span>
+              </Tooltip>
+            </span>
+          </Checkbox>
+        </CheckboxGroup>
+      </div>
+
+      <Divider className="my-4" />
+
+      <CheckboxGroup
+        label="Accent"
+        onChange={(values) => handleFilterChange('selectedAccent', values)}
+        orientation="horizontal"
+        size="sm"
+        radius="full"
+        value={filters.selectedAccent}
+      >
+        {accent.map((accent) => (
+          <Checkbox key={accent} value={accent}>
+            <Tooltip content={accent}>
+              <span className="text-3xl">{getAccentEmoji(accent)}</span>
+            </Tooltip>
+          </Checkbox>
+        ))}
+      </CheckboxGroup>
+
+      <Divider className="my-2" />
+
+      <CheckboxGroup
+        label="Use Case"
+        onChange={(values) => handleFilterChange('selectedUseCase', values)}
+        orientation="horizontal"
+        size="sm"
+        radius="full"
+        value={filters.selectedUseCase}
+      >
+        {useCase.sort().map((useCase) => (
+          <Checkbox key={useCase} value={useCase}>
+            {capitalize(useCase)}
+          </Checkbox>
+        ))}
+      </CheckboxGroup>
+
+      <Divider className="my-2" />
+
+      <CheckboxGroup
+        label="Description"
+        onChange={(values) => handleFilterChange('selectedDescription', values)}
+        orientation="horizontal"
+        size="sm"
+        radius="full"
+        value={filters.selectedDescription}
+      >
+        {description.sort().map((description) => (
+          <Checkbox key={description} value={description}>
+            {capitalize(description)}
+          </Checkbox>
+        ))}
+      </CheckboxGroup>
+    </div>
+  );
+}
