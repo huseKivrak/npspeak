055ec5839e25f26abb03b3287bfeb5a456715a7f
Huse Kivrak <huse7@pm.me>
2024-07-12 16:33:00 -0400

    chore - updated for ^0.21 config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,20 +6,13 @@ import { defineConfig } from 'drizzle-kit';
 
 loadEnvConfig(cwd());
 
-const pw = process.env.DATABASE_PASSWORD || '';
-const dbHost = process.env.DATABASE_HOST || '';
-const dbUser = process.env.DATABASE_USER || '';
 export default defineConfig({
   schema: './database/drizzle/schema.ts',
   schemaFilter: ['public'],
   out: './database/drizzle',
   dialect: 'postgresql',
   dbCredentials: {
-    user: dbUser,
-    password: pw,
-    host: dbHost,
-    port: 6543,
-    database: 'postgres',
+    url: process.env.POSTGRES_URL!,
   },
   introspect: {
     casing: 'preserve',
