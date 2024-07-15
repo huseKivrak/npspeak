fcd6fb0b886563cab13b23514bd49615b6bad515
Huse Kivrak <huse7@pm.me>
2024-07-15 14:59:00 -0400

    refactor

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,19 +1,21 @@
+import { NextRequest, NextResponse } from 'next/server';
 import { createClientOnServer } from '@/utils/supabase/server';
-import { NextResponse } from 'next/server';
-import { cookies } from 'next/headers';
 
-export async function GET(request: Request) {
+export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get('code');
-  const origin = requestUrl.origin;
 
   if (code) {
     const supabase = createClientOnServer();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
-    if (!error) {
-      return NextResponse.redirect(`${origin}/dashboard`);
+
+    if (error) {
+      return NextResponse.redirect(
+        `${requestUrl.origin}/login?message=auth-code-error`
+      );
     }
   }
 
-  return NextResponse.redirect(`${origin}/login?message=auth-code-error`);
+  // URL to redirect to after sign in process completes
+  return NextResponse.redirect(`${requestUrl.origin}/dashboard`);
 }
