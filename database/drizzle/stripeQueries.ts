149aaed2f755f9a6bf8c82bd858ade06f3f83a79
Huse Kivrak <huse7@pm.me>
2024-07-15 18:19:52 -0400

    refactor - added product query & prices to subscription query

--- database/drizzle/stripeQueries.ts
+++ database/drizzle/stripeQueries.ts
@@ -1,11 +1,27 @@
 import { eq } from 'drizzle-orm';
 import { db } from './index';
-import { subscriptions } from './schema';
+import { subscriptions, products, prices } from './schema';
 
 export const getSubscription = async (userId: string) => {
   const subscription = await db.query.subscriptions.findFirst({
     where: eq(subscriptions.user_id, userId),
+    with: {
+      price: true,
+    },
   });
 
   return subscription;
+};
+
+export const getProducts = async () => {
+  const activeProducts = await db.query.products.findMany({
+    where: eq(products.active, true),
+    with: {
+      prices: {
+        where: eq(prices.active, true),
+      },
+    },
+  });
+
+  return activeProducts;
 };
