69036bfdf5298789dc8fb57e0f1840023048d378
Huse Kivrak <huse7@pm.me>
2024-07-15 16:43:50 -0400

    debug - testing stripe

--- middleware.ts
+++ middleware.ts
@@ -6,6 +6,6 @@ export async function middleware(request: NextRequest) {
 }
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|signup|login|about|contact).*)',
+    '/((?!_next/static|_next/image|favicon.ico|signup|login|about|contact|api).*)',
   ],
 };
