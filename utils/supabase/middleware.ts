fe485fc1084d49dd74f5f0de586e415a587a9908
Huse Kivrak <huse7@pm.me>
2024-07-17 17:33:59 -0400

    added missing route

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -39,6 +39,7 @@ export async function updateSession(request: NextRequest) {
     '/about',
     '/contact',
     '/api',
+    '/auth',
     '/forgot-password',
   ];
   const isUnprotectedPath = unprotectedPaths.some((path) =>
