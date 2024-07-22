297fe3f1fadde841564cf1407c82b75d7e5ff132
Huse Kivrak <huse7@pm.me>
2024-07-22 16:10:26 -0400

    file - added red hat text and mono fonts

--- config/fonts.ts
+++ config/fonts.ts
@@ -1,5 +1,6 @@
-import { Grenze as FontGrenze } from 'next/font/google';
+import { Grenze as FontGrenze, Red_Hat_Mono } from 'next/font/google';
 import localFont from 'next/font/local';
+import { Red_Hat_Text } from 'next/font/google';
 
 export const fontGrenze = FontGrenze({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
@@ -11,4 +12,15 @@ export const fontGrenze = FontGrenze({
 export const fontAlagard = localFont({
   src: '../public/fonts/alagard12.otf',
   variable: '--font-alagard',
+});
+
+export const redHatText = Red_Hat_Text({
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+export const redHatMono = Red_Hat_Mono({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-mono',
 });
