a7178800653ea1afb72cf70cc168ace57177f8a9
Huse Kivrak <huse7@pm.me>
2024-09-16 19:14:43 -0400

    feat: form for encapsulated promo code handling; WIP

---
+++ components/forms/PromoForm.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import { useState } from 'react';
+import { getPromoCodeStatus } from '@/actions/db/promo';
+import { Button, Input } from '@nextui-org/react';
+import { FaRegArrowAltCircleRight } from 'react-icons/fa';
+
+interface PromoFormProps {
+  onValidPromoCode: (code: string) => void;
+}
+export default function PromoForm({ onValidPromoCode }: PromoFormProps) {
+  const [promoCode, setPromoCode] = useState('');
+  const [error, setError] = useState('');
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+    const codeStatus = await getPromoCodeStatus(promoCode);
+    if (codeStatus.status === 'error') {
+      setError(codeStatus.message);
+      setIsSubmitting(false);
+    } else {
+      setError('');
+      onValidPromoCode(promoCode);
+      setIsSubmitting(false);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="flex gap-2">
+      <Input
+        type="text"
+        aria-label="promo code"
+        value={promoCode}
+        placeholder="enter code"
+        onChange={(e) => setPromoCode(e.target.value)}
+        isInvalid={!!error}
+        errorMessage={error}
+        size="lg"
+      />
+      <Button
+        color={error ? 'danger' : 'success'}
+        isLoading={isSubmitting}
+        isIconOnly
+        type="submit"
+        aria-label="apply code"
+        size="lg"
+      >
+        <FaRegArrowAltCircleRight />
+      </Button>
+    </form>
+  );
+}
