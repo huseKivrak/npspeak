79bd1dd11195f48a5a1163f21d9c94f003150b9c
Huse Kivrak <huse7@pm.me>
2024-09-17 19:44:33 -0400

    minor: WIP

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,29 +1,3 @@
 export default function AboutPage() {
-  return (
-    <div className="flex flex-col items-center px-36">
-      <h1 className="text-6xl md:text-8xl lg:text-9xl font-thin font-alagard">
-        About
-      </h1>
-      <p>
-        Quis, anim excepteur reprehenderit veniam sit. Reprehenderit veniam sit
-        eu duis nisi consectetur voluptate. Sit eu duis nisi consectetur,
-        voluptate pariatur. Nisi consectetur voluptate pariatur. Voluptate
-        pariatur quis labore consectetur aute. Quis labore consectetur aute anim
-        enim deserunt sint. Consectetur, aute anim enim. Enim deserunt sint
-        magna esse. Sint, magna esse minim sed amet in.
-      </p>
-      <h2 className="text-3xl md:text-5xl lg:text-6xl font-alagard text-secondary">
-        FAQ
-      </h2>
-      <p>
-        Enim ullamco est ipsum. Est ipsum sit ex, proident ullamco nulla. Ex
-        proident ullamco nulla, eiusmod. Nulla eiusmod commodo velit ea eiusmod
-        proident. Commodo velit ea eiusmod proident. Ea eiusmod proident nulla,
-        lorem culpa. Nulla lorem culpa ad, quis sed veniam ex. Ad quis sed
-        veniam ex. Sed veniam ex ut, ex eu. Ut ex eu eu ipsum sunt adipiscing
-        adipiscing.
-      </p>
-      <p>For more information or support, please contact us at</p>
-    </div>
-  );
+  return <div className="flex flex-col items-center px-36"></div>;
 }
