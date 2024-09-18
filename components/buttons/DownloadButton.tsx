0edf9690982ef8eb8d5af26909a57018386a4ae6
Huse Kivrak <huse7@pm.me>
2024-09-18 12:15:31 -0400

    feat: WIP

---
+++ components/buttons/DownloadButton.tsx
@@ -0,0 +1,33 @@
+import { DialogueRow } from '@/types/drizzle';
+import { Button, Tooltip } from '@nextui-org/react';
+import { FaDownload } from 'react-icons/fa';
+
+export function DownloadButton({ dialogue }: { dialogue: DialogueRow }) {
+  const handleDownload = () => {
+    if (dialogue.audio) {
+      const link = document.createElement('a');
+      link.href = dialogue.audio;
+      link.download = `${dialogue.type}_${dialogue.id}.mp3`;
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
+  };
+
+  return (
+    <>
+      <Tooltip content="Download Audio">
+        <Button
+          isIconOnly
+          variant="light"
+          color="success"
+          size="sm"
+          radius="full"
+          onClick={handleDownload}
+        >
+          <FaDownload size={18} />
+        </Button>
+      </Tooltip>
+    </>
+  );
+}
