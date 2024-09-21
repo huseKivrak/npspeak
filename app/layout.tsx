744bae1a8e5f74d54ff4d064cc878598c43caadb
Huse Kivrak <huse7@pm.me>
2024-09-20 20:01:34 -0400

    refactor / styling: consolidated fonts import, simplified layout padding/styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,7 @@ import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Providers } from './providers';
 import { siteConfig } from '@/config/site';
-import {
-  fontAlagard,
-  fontGrenze,
-  redHatText,
-  redHatMono,
-} from '@/config/fonts';
+import { fonts } from '@/config/fonts';
 
 import clsx from 'clsx';
 import { Navbar } from '@/components/layout/Navbar';
@@ -40,10 +35,7 @@ export default async function RootLayout({ children }: PropsWithChildren) {
       <body
         className={clsx(
           'min-h-screen text-foreground bg-background font-rht antialiased',
-          fontGrenze.variable,
-          fontAlagard.variable,
-          redHatText.variable,
-          redHatMono.variable
+          fonts
         )}
       >
         <Providers>
@@ -51,12 +43,12 @@ export default async function RootLayout({ children }: PropsWithChildren) {
             <Navbar>
               <AuthButton />
             </Navbar>
-            <main className="container mx-auto max-w-4xl md:max-w-7xl px-4 sm:px-8 flex-grow">
+            <main className="container mx-auto px-8 pt-2 sm:pt-8 flex-grow">
               {children}
             </main>
             <Analytics />
             <SpeedInsights />
-            <footer className="mx-auto max-w-[1920px] flex items-center justify-center px-4 py-4 md:py-8 lg:py-12">
+            <footer className="container mx-auto flex items-center justify-center px-4 py-8 lg:py-12">
               <span className="sm:text-small md:text-base lg:text-large tracking-wider">
                 &copy; {new Date().getFullYear()} npSpeak. All rights reserved.
               </span>
--- config/fonts.ts
+++ config/fonts.ts
@@ -1,27 +1,27 @@
-import { Grenze as FontGrenze, Red_Hat_Mono } from 'next/font/google';
+import { Red_Hat_Mono } from 'next/font/google';
 import localFont from 'next/font/local';
 import { Red_Hat_Text } from 'next/font/google';
 
-export const fontGrenze = FontGrenze({
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  style: ['normal', 'italic'],
-  subsets: ['latin'],
-  variable: '--font-grenze',
-});
-
-export const fontAlagard = localFont({
+const fontAlagard = localFont({
   src: '../public/fonts/alagard12.otf',
   variable: '--font-alagard',
+  display: 'swap',
 });
 
-export const redHatText = Red_Hat_Text({
+const redHatText = Red_Hat_Text({
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-rht',
 });
 
-export const redHatMono = Red_Hat_Mono({
+const redHatMono = Red_Hat_Mono({
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-mono',
 });
+
+export const fonts = [
+  fontAlagard.variable,
+  redHatText.variable,
+  redHatMono.variable,
+];
