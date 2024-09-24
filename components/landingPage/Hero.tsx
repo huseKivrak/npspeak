e014149323348df3d40fe0328b6ab79afc2273cf
Huse Kivrak <huse7@pm.me>
2024-09-24 14:24:13 -0400

    styling: WIP - playing with layout, animation, etc. WIP

--- components/landingPage/Hero.tsx
+++ components/landingPage/Hero.tsx
@@ -4,27 +4,54 @@ import { Button } from '@nextui-org/button';
 import { Link } from '@nextui-org/react';
 import { FaArrowRight } from 'react-icons/fa6';
 
+
 export const Hero = () => {
   return (
-    <div className="flex flex-col gap-8">
-      <h1 className="fadeInFirst text-6xl md:text-8xl lg:text-9xl font-bold tracking-wider">
+    <div className="flex flex-col gap-6">
+      <h1 className=" text-6xl md:text-8xl lg:text-9xl font-bold tracking-wider">
         npSpeak
       </h1>
 
-      <p className="fadeInSecond font-bold text-warning text-2xl md:text-4xl lg:text-5xl">
+      <h2 className=" font-bold text-warning text-2xl md:text-4xl lg:text-5xl">
         Give your character a voice.
-      </p>
+      </h2>
+
+
 
       <Link href="/signup">
         <Button
           variant="flat"
-          className="fadeInThirdUp md:h-14 md:text-xl"
+          className=" md:h-14 md:text-2xl"
           color="success"
+          size="lg"
           endContent={<FaArrowRight />}
         >
           Get Started
         </Button>
       </Link>
+
+
+      <div className='flex flex-col'>
+        <h2>AI-powered voice synthesis</h2>
+        <p className='max-w-xl'>
+          Est aliqua magna eiusmod est.
+          Magna eiusmod est excepteur voluptate et.
+          Est, excepteur voluptate et aliquip amet aliquip.
+          Et, aliquip amet aliquip.
+          Amet, enim voluptate eiusmod.
+        </p>
+
+        <h2>
+          everything you need.
+          <br />
+          nothing you don&apos;t.
+        </h2>
+        <p className='max-w-xl'>
+
+        </p>
+
+        <h2></h2>
+      </div>
     </div>
   );
 };
