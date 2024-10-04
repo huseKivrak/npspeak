8ba45449926c761ee8bcad6a796b07c5356e4409
Huse Kivrak <huse7@pm.me>
2024-10-04 17:39:25 -0400

    refactor: reverted box shadows, temporarily duplicated dark theme for light them for now; WIP

--- styles/theme.ts
+++ styles/theme.ts
@@ -118,6 +118,133 @@ const themeColors: ConfigThemes = {
   //     },
   //   },
   // },
+  light: {
+    colors: {
+      background: {
+        DEFAULT: '#111315',
+      },
+      foreground: {
+        DEFAULT: '#FDF2E4',
+        50: '#5A3607',
+        100: '#B46C0D',
+        200: '#F09E33',
+        300: '#F6C788',
+        400: '#FDF2E4',
+        500: '#FDF4E7',
+        600: '#FEF8F1',
+        700: '#FEFAF6',
+        800: '#FFFDFA',
+        900: '#FFFDFA',
+      },
+      divider: {
+        DEFAULT: '#1d2023',
+      },
+      overlay: {
+        DEFAULT: '#4a4d4f',
+      },
+      focus: {
+        DEFAULT: '#5c8b93',
+      },
+      content1: {
+        DEFAULT: '#171a1c',
+        foreground: '#FDF2E4',
+      },
+      content2: {
+        DEFAULT: '#1d2023',
+        foreground: '#FDF2E4',
+      },
+      content3: {
+        DEFAULT: '#4a4d4f',
+        foreground: '#FDF2E4',
+      },
+      content4: {
+        DEFAULT: '#77797b',
+        foreground: '#FDF2E4',
+      },
+
+      default: {
+        foreground: '#FDF2E4',
+        DEFAULT: '#4a4d4f',
+        50: '#171A1C',
+        100: '#1d2023',
+        200: '#4a4d4f',
+        300: '#77797b',
+        400: '#a0a2a4',
+        500: '#c8c9cb',
+        600: '#e0e1e2',
+        700: '#f0f1f2',
+        800: '#f8f9fa',
+        900: '#ffffff',
+      },
+      primary: {
+        50: '#171A1C',
+        100: '#171A1C',
+        200: '#151719',
+        300: '#121516',
+        400: '#101214',
+        500: '#0E1011',
+        600: '#0C0D0E',
+        700: '#090A0B',
+        800: '#070808',
+        900: '#050506',
+        DEFAULT: '#5E8AC0',
+      },
+
+      secondary: {
+        50: '#372032',
+        100: '#58324F',
+        200: '#683B5E',
+        300: '#7B4770',
+        400: '#8A4F7D',
+        500: '#B884AD',
+        600: '#CFABC7',
+        700: '#E3CEDF',
+        800: '#F2E8F0',
+        900: '#F8F2F6',
+        DEFAULT: '#B884AD',
+      },
+      success: {
+        50: '#192A23',
+        100: '#284338',
+        200: '#2F5043',
+        300: '#375D4E',
+        400: '#3E6958',
+        500: '#5FA086',
+        600: '#95C0AF',
+        700: '#BFD9CF',
+        800: '#DFECE7',
+        900: '#EFF6F3',
+        DEFAULT: '#5FA086',
+        foreground: '#FDF2E4',
+      },
+      warning: {
+        50: '#75611A',
+        100: '#9B8122',
+        200: '#BC9C29',
+        300: '#D5B43F',
+        400: '#E0C872',
+        500: '#E7D592',
+        600: '#EEE0B0',
+        700: '#F4EBCD',
+        800: '#FAF7EA',
+        900: '#FCFAF2',
+        DEFAULT: '#E7D592',
+      },
+      danger: {
+        50: '#581D24',
+        100: '#6F252D',
+        200: '#8E2F3A',
+        300: '#9D3440',
+        400: '#B03A48',
+        500: '#D47D87',
+        600: '#E3ABB1',
+        700: '#EDC9CE',
+        800: '#F7E8EA',
+        900: '#FBF3F4',
+        DEFAULT: '#D47D87',
+      },
+    },
+  },
   dark: {
     colors: {
       background: {
@@ -289,12 +416,15 @@ const themeLayout: LayoutTheme = {
 
   //default box shadows
   boxShadow: {
-    //shadow-small
-    small: '3px 3px 0px 0px #000',
-    //shadow-medium
-    medium: '6px 6px 0px 0px #000',
-    //shadow-large
-    large: '9px 9px 0px 0px #000',
+    // shadow-small
+    small:
+      '0px 0px 5px 0px rgb(0 0 0 / 0.05), 0px 2px 10px 0px rgb(0 0 0 / 0.2), inset 0px 0px 1px 0px rgb(255 255 255 / 0.15)',
+    // shadow-medium
+    medium:
+      '0px 0px 15px 0px rgb(0 0 0 / 0.06), 0px 2px 30px 0px rgb(0 0 0 / 0.22), inset 0px 0px 1px 0px rgb(255 255 255 / 0.15)',
+    // shadow-large
+    large:
+      '0px 0px 30px 0px rgb(0 0 0 / 0.07), 0px 30px 60px 0px rgb(0 0 0 / 0.26), inset 0px 0px 1px 0px rgb(255 255 255 / 0.15)',
   },
 };
 
