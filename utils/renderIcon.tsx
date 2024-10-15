749a3cd36dce854a6ea704dd73a38edff4b388e4
Huse Kivrak <huse7@pm.me>
2024-10-15 18:03:40 -0400

    refactor: added isDialogue prop and color map for dialogue icon styling

--- utils/renderIcon.tsx
+++ utils/renderIcon.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { iconMap } from '@/lib/iconMap';
+import { cn } from './helpers/clsxMerge';
 
 interface RenderIconProps {
   iconName: string;
+  isDialogue?: boolean;
   className?: string;
   [ key: string ]: any;
 }
 
 export const RenderIcon: React.FC<RenderIconProps> = ({
   iconName,
-
+  isDialogue = false,
   className,
   ...props
 }) => {
@@ -20,9 +22,21 @@ export const RenderIcon: React.FC<RenderIconProps> = ({
     return null;
   }
 
+  const dialogueColorMap: { [ key: string ]: string; } = {
+    greeting: 'text-primary',
+    farewell: 'text-danger',
+    story: 'text-secondary',
+    question: 'text-foreground',
+    answer: 'text-success',
+    exclamation: 'text-warning',
+    other: 'text-default',
+  };
+
+  const iconColor = isDialogue ? dialogueColorMap[ iconName ] : '';
+
   return (
     <IconComponent
-      className={className}
+      className={cn(className, iconColor)}
       {...props}
     />
   );
