217717f6d2ea6d1a52c1c91c8b45bafd07a080f9
Huse Kivrak <huse7@pm.me>
2024-10-15 18:01:47 -0400

    refactor: modified handleDownload to ensure link opens in new tab; WIP

--- components/buttons/DownloadButton.tsx
+++ components/buttons/DownloadButton.tsx
@@ -5,28 +5,22 @@ import { FaDownload } from 'react-icons/fa';
 export function DownloadButton({ dialogue }: { dialogue: DialogueRow; }) {
   const handleDownload = () => {
     if (dialogue.audio) {
-      const link = document.createElement('a');
-      link.href = dialogue.audio;
-      link.download = `${dialogue.type}_${dialogue.id}.mp3`;
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
+      window.open(dialogue.audio, '_blank');
     }
   };
 
   return (
-    <>
-      <Tooltip content="Download Audio">
-        <Button
-          isIconOnly
-          variant="light"
-          size="lg"
-          radius="full"
-          onClick={handleDownload}
-        >
-          <FaDownload size={20} />
-        </Button>
-      </Tooltip>
-    </>
+    <Tooltip content="Download Audio">
+      <Button
+        isIconOnly
+        variant="light"
+        size="lg"
+        radius="full"
+        onClick={handleDownload}
+      >
+        <FaDownload size={20} />
+      </Button>
+    </Tooltip>
+
   );
 }
