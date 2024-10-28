c2ff710c8937b90bd0d5d7774cd39f7d171af2cf
Huse Kivrak <huse7@pm.me>
2024-10-28 18:58:37 -0400

    refactor/feat : WIP

--- components/views/CampaignDetail.tsx
+++ components/views/CampaignDetail.tsx
@@ -1,16 +1,26 @@
-import { CampaignWithNPCs } from '@/types/drizzle';
+'use client';
+
+import { CampaignWithDetailedNPCs } from '@/types/drizzle';
+import { Divider } from '@nextui-org/react';
+import { NPCListTable } from '../tables/NPCListTable';
+
 
 export const CampaignDetail = ({
   campaign,
 }: {
-  campaign: CampaignWithNPCs;
+  campaign: CampaignWithDetailedNPCs;
 }) => {
   return (
     <div className="flex flex-col w-full">
       <div className="flex flex-col gap-2">
-        <h1>{campaign.campaign_name}</h1>
 
-        <p>{campaign.description}</p>
+        <h1>{campaign.campaign_name}</h1>
+        <span className="font-mono max-w-2xl">{campaign.description}</span>
+
+        <Divider className='h-[.5px] bg-foreground my-2' />
+        <h2>NPCs</h2>
+
+        <NPCListTable npcs={campaign.npcs || []} />
       </div>
     </div>
   );
