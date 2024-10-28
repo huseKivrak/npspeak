ce1d60c8d60ff75da5becf413508436e60af1b79
Huse Kivrak <huse7@pm.me>
2024-10-28 18:11:49 -0400

    refactor: removed redundant auth check

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,15 +1,10 @@
-import { getUserProfile } from '@/actions/auth';
-import { redirect } from 'next/navigation';
 
 export default async function ProtectedLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const { user } = await getUserProfile();
-  if (!user) {
-    return redirect('/login');
-  }
+
 
   return <>{children}</>;
 }
