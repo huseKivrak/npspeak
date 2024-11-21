ffd70952d9ec67b9781b8500c691ef07e87ab21c
Huse Kivrak <huse7@pm.me>
2024-11-21 16:14:50 -0500

    refactor: improved email confirmation page

--- app/signup/success/page.tsx
+++ app/signup/success/page.tsx
@@ -1,11 +1,21 @@
+import verifyScroll from '@/public/images/verify_scroll.png';
+import Image from 'next/image';
 export default function SignupSuccessPage() {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <h1>Account created!</h1>
-      <p className="text-lg text-primary">
-        Please check your email for the confirmation link and start using
-        npSpeak
-      </p>
+    <div className="flex flex-col items-center justify-center gap-4">
+
+      <h1 className='text-center'>an email awaits</h1>
+      <Image
+        src={verifyScroll}
+        alt='verify email'
+        width={200}
+        height={200}
+        className='mx-auto'
+      />
+
+      <span className="text-xl sm:text-4xl text-balance text-center">
+        please verify your email to start using npSpeak
+      </span>
     </div>
   );
 }
